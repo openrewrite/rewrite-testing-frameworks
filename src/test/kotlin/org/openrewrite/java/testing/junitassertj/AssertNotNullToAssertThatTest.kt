05e920929e2127f97ff581ae9047d2a9ad36414b
Sofia Britto Schwartz <sofia.b.schwartz@gmail.com>
2020-12-10 12:07:03 -0800

    JUnit 5 assertNull / assertNotNull conversions to assertJ (#18)

---
+++ src/test/kotlin/org/openrewrite/java/testing/junitassertj/AssertNotNullToAssertThatTest.kt
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.junitassertj
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Parser
+import org.openrewrite.RefactorVisitor
+import org.openrewrite.RefactorVisitorTestForParser
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.tree.J
+
+class AssertNotNullToAssertThatTest : RefactorVisitorTestForParser<J.CompilationUnit> {
+    override val parser: Parser<J.CompilationUnit> = JavaParser.fromJavaVersion()
+        .classpath("junit", "assertj-core", "apiguardian-api")
+        .build()
+
+    override val visitors: Iterable<RefactorVisitor<*>> = listOf(AssertNotNullToAssertThat())
+
+    @Test
+    fun singleStaticMethodNoMessage() = assertRefactored(
+        before = """
+                import org.junit.Test;
+
+                import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+                public class A {
+ 
+                    @Test
+                    public void test() {
+                        assertNotNull(notification());
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """,
+        after = """
+                import org.junit.Test;
+
+                import static org.assertj.core.api.Assertions.assertThat;
+
+                public class A {
+
+                    @Test
+                    public void test() {
+                        assertThat(notification()).isNotNull();
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """
+    )
+
+    @Test
+    fun singleStaticMethodWithMessageString() = assertRefactored(
+        before = """
+                import org.junit.Test;
+
+                import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+                public class A {
+ 
+                    @Test
+                    public void test() {
+                        assertNotNull(notification(), "Should not be null");
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """,
+        after = """
+                import org.junit.Test;
+
+                import static org.assertj.core.api.Assertions.assertThat;
+
+                public class A {
+
+                    @Test
+                    public void test() {
+                        assertThat(notification()).as("Should not be null").isNotNull();
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """
+    )
+
+    @Test
+    fun singleStaticMethodWithMessageSupplier() = assertRefactored(
+        before = """
+                import org.junit.Test;
+
+                import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+                public class A {
+ 
+                    @Test
+                    public void test() {
+                        assertNotNull(notification(), () -> "Should not be null");
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """,
+        after = """
+                import org.junit.Test;
+
+                import static org.assertj.core.api.Assertions.assertThat;
+
+                public class A {
+
+                    @Test
+                    public void test() {
+                        assertThat(notification()).withFailMessage(() -> "Should not be null").isNotNull();
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """
+    )
+
+    @Test
+    fun inlineReference() = assertRefactored(
+        before = """
+                import org.junit.Test;
+ 
+                public class A {
+                
+                    @Test
+                    public void test() {
+                        org.junit.jupiter.api.Assertions.assertNotNull(notification());
+                        org.junit.jupiter.api.Assertions.assertNotNull(notification(), "Should not be null");
+                        org.junit.jupiter.api.Assertions.assertNotNull(notification(), () -> "Should not be null");
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """,
+        after = """
+                import org.junit.Test;
+                
+                import static org.assertj.core.api.Assertions.assertThat;
+                
+                public class A {
+                
+                    @Test
+                    public void test() {
+                        assertThat(notification()).isNotNull();
+                        assertThat(notification()).as("Should not be null").isNotNull();
+                        assertThat(notification()).withFailMessage(() -> "Should not be null").isNotNull();
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """
+    )
+
+    @Test
+    fun mixedReferences() = assertRefactored(
+        before = """
+                import org.junit.Test;
+                
+                import static org.assertj.core.api.Assertions.*;
+                import static org.junit.jupiter.api.Assertions.assertNotNull;
+                
+                public class A {
+                
+                    @Test
+                    public void test() {
+                        assertNotNull(notification());
+                        org.junit.jupiter.api.Assertions.assertNotNull(notification(), "Should not be null");
+                        assertNotNull(notification(), () -> "Should not be null");
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """,
+        after = """
+                import org.junit.Test;
+                
+                import static org.assertj.core.api.Assertions.*;
+                
+                public class A {
+                
+                    @Test
+                    public void test() {
+                        assertThat(notification()).isNotNull();
+                        assertThat(notification()).as("Should not be null").isNotNull();
+                        assertThat(notification()).withFailMessage(() -> "Should not be null").isNotNull();
+                    }
+                    private String notification() {
+                        return "";
+                    }
+                }
+            """
+    )
+}
