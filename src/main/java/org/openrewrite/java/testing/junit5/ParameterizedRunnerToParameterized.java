7fcee69c4cc6e513f5c11545b9b913bf0ef09261
Patrick Way <pway99@users.noreply.github.com>
2021-03-24 11:07:54 -0700

    Add ParameterizedRunnerToParameterized to JUnit4To5Migration and some polish (#90)

--- src/main/java/org/openrewrite/java/testing/junit5/ParameterizedRunnerToParameterized.java
+++ src/main/java/org/openrewrite/java/testing/junit5/ParameterizedRunnerToParameterized.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.junit5;
 
 import org.openrewrite.Cursor;
@@ -18,7 +33,9 @@ import java.util.stream.Collectors;
 import static org.openrewrite.Tree.randomId;
 
 /**
- * Recipe for converting @RunWith(@Parameterized.class) to @ParameterizedTests with @MethodSource
+ * Recipe for migrating JUnit4 @RunWith(Parameterized.class) tests to their JUnit 5 Jupiter ParameterizedTest equivalent.
+ * <p>
+ * Visitor for converting @RunWith(@Parameterized.class) to @ParameterizedTests with @MethodSource
  * <p>
  * 1. Remove `@RunWith(Parameterized.class)`
  * 2. Replace `@Test` with `@ParameterizedTest` having arguments from `@Parameters` method
