82590fdbe4a5f1c103b0735ec47689c073660b0b
Aaron Gershman <aegershman@gmail.com>
2021-03-24 14:21:57 -0500

    Performance enhancements for UpdateTestAnnotation [#392]

--- src/main/java/org/openrewrite/java/testing/junit5/UpdateTestAnnotation.java
+++ src/main/java/org/openrewrite/java/testing/junit5/UpdateTestAnnotation.java
@@ -23,6 +23,7 @@ import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.ChangeType;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.search.FindTypes;
 import org.openrewrite.java.tree.*;
 
 import java.util.ArrayList;
@@ -51,7 +52,9 @@ public class UpdateTestAnnotation extends Recipe {
 
         @Override
         public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext ctx) {
-            doAfterVisit(new ChangeType("org.junit.Test", "org.junit.jupiter.api.Test"));
+            if (!FindTypes.find(cu, "org.junit.Test").isEmpty()) {
+                doAfterVisit(new ChangeType("org.junit.Test", "org.junit.jupiter.api.Test"));
+            }
             return super.visitCompilationUnit(cu, ctx);
         }
 
