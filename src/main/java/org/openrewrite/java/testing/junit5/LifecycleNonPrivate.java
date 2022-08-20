4e5955d4642f6db0499b91664f95a222f6673e81
Tim te Beek <tim.te.beek@jdriven.com>
2022-08-20 16:30:25 +0200

    Add LifecycleNonPrivate for JUnit 5.9.0 (#241) (#252)
    * Add LifecycleNonPrivate for JUnit 5.9.0 (#241)
    * Add TempDirNonFinal for JUnit 5.9.0 (#241)

---
+++ src/main/java/org/openrewrite/java/testing/junit5/LifecycleNonPrivate.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.junit5;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.J.MethodDeclaration;
+import org.openrewrite.java.tree.J.Modifier.Type;
+import org.openrewrite.java.tree.JavaSourceFile;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+public class LifecycleNonPrivate extends Recipe {
+
+    private static final List<String> ANNOTATIONS = Arrays.asList(
+            "org.junit.jupiter.api.AfterAll",
+            "org.junit.jupiter.api.AfterEach",
+            "org.junit.jupiter.api.BeforeAll",
+            "org.junit.jupiter.api.BeforeEach");
+
+    @Override
+    public String getDisplayName() {
+        return "Make lifecycle methods non private";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Make JUnit 5's `@AfterAll`, `@AfterEach`, `@BeforeAll` and `@BeforeEach` non private.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitJavaSourceFile(JavaSourceFile cu, ExecutionContext executionContext) {
+                ANNOTATIONS.forEach(ann -> doAfterVisit(new UsesType<>(ann)));
+                return cu;
+            }
+        };
+    }
+
+    @Override
+    protected LifecycleNonPrivateVisitor getVisitor() {
+        return new LifecycleNonPrivateVisitor();
+    }
+
+    private static class LifecycleNonPrivateVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(MethodDeclaration method, ExecutionContext p) {
+            J.MethodDeclaration md = super.visitMethodDeclaration(method, p);
+            if (md.getModifiers().stream().anyMatch(mod -> mod.getType() == Type.Private)
+                    && md.getLeadingAnnotations().stream().anyMatch(ann -> ANNOTATIONS.stream()
+                            .map(AnnotationMatcher::new)
+                            .anyMatch(matcher -> matcher.matches(ann)))) {
+                return maybeAutoFormat(md,
+                        md.withModifiers(ListUtils.map(md.getModifiers(),
+                                modifier -> modifier.getType() == Type.Private ? null : modifier)),
+                        p, getCursor().getParent());
+            }
+            return md;
+        }
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(1);
+    }
+
+}
