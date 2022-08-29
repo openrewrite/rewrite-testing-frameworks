dcf156e8da4d1614937fa5fedadd10c91e9c3943
Tim te Beek <tim.te.beek@jdriven.com>
2022-08-29 17:01:33 +0200

    Add missing @Nested annotation (#257)
    Fixes #215

---
+++ src/main/java/org/openrewrite/java/testing/junit5/AddMissingNested.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.junit5;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.FindAnnotations;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaSourceFile;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.time.Duration;
+import java.util.*;
+
+import static org.openrewrite.Parser.Input.fromString;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class AddMissingNested extends Recipe {
+    private static final String NESTED = "org.junit.jupiter.api.Nested";
+    private static final List<String> TEST_ANNOTATIONS = Arrays.asList(
+            "org.junit.jupiter.api.Test",
+            "org.junit.jupiter.api.TestTemplate",
+            "org.junit.jupiter.api.RepeatedTest",
+            "org.junit.jupiter.params.ParameterizedTest",
+            "org.junit.jupiter.api.TestFactory");
+
+    @Override
+    public String getDisplayName() {
+        return "JUnit5 inner test classes should be annotated with `@Nested`.";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds `@Nested` to inner classes that contain JUnit 5 tests.";
+    }
+
+    @Override
+    protected @Nullable TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitJavaSourceFile(JavaSourceFile cu, ExecutionContext executionContext) {
+                TEST_ANNOTATIONS.forEach(ann -> doAfterVisit(new UsesType<>(ann)));
+                return cu;
+            }
+        };
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(1);
+    }
+
+    @Override
+    public Set<String> getTags() {
+        return Collections.singleton("RSPEC-5790");
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+                cd = cd.withBody((J.Block) new AddNestedAnnotationVisitor().visit(cd.getBody(), ctx, getCursor()));
+                maybeAddImport(NESTED);
+                return cd;
+            }
+        };
+    }
+
+    public static class AddNestedAnnotationVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+            boolean alreadyNested = classDecl.getLeadingAnnotations().stream()
+                    .anyMatch(a -> TypeUtils.isOfClassType(a.getType(), NESTED));
+            if (!alreadyNested && hasTestMethods(cd)) {
+                cd = cd.withTemplate(getNestedJavaTemplate(), cd.getCoordinates().addAnnotation(Comparator.comparing(
+                        J.Annotation::getSimpleName)));
+                cd.getModifiers().removeIf(modifier -> modifier.getType().equals(J.Modifier.Type.Static));
+            }
+            return cd;
+        }
+
+        @NotNull
+        private JavaTemplate getNestedJavaTemplate() {
+            return JavaTemplate.builder(this::getCursor, "@Nested")
+                    .javaParser(() -> JavaParser.fromJavaVersion().dependsOn(Collections.singletonList(
+                            fromString("package org.junit.jupiter.api;\npublic @interface Nested {}"))).build())
+                    .imports(NESTED).build();
+        }
+
+        private static boolean hasTestMethods(final J.ClassDeclaration cd) {
+            return TEST_ANNOTATIONS.stream().anyMatch(ann -> !FindAnnotations.find(cd, "@" + ann).isEmpty());
+        }
+    }
+}
