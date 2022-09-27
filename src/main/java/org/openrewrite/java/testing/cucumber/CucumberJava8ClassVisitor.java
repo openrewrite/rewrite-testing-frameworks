a753bba5993a21144638d22fdbcea050c7d9e17c
Tim te Beek <tim.te.beek@jdriven.com>
2022-09-27 14:27:32 +0300

    Add missing header & drop Java 11 List.of()

--- src/main/java/org/openrewrite/java/testing/cucumber/CucumberJava8ClassVisitor.java
+++ src/main/java/org/openrewrite/java/testing/cucumber/CucumberJava8ClassVisitor.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.cucumber;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -72,7 +88,8 @@ class CucumberJava8ClassVisitor extends JavaIsoVisitor<ExecutionContext> {
 
     private List<TypeTree> filterImplementingInterfaces(J.ClassDeclaration classDeclaration) {
         List<TypeTree> retained = new ArrayList<>();
-        for (TypeTree typeTree : Optional.ofNullable(classDeclaration.getImplements()).orElse(List.of())) {
+        for (TypeTree typeTree : Optional.ofNullable(classDeclaration.getImplements())
+                .orElse(Collections.emptyList())) {
             if (typeTree.getType() instanceof JavaType.Class) {
                 JavaType.Class clazz = (Class) typeTree.getType();
                 if (IO_CUCUMBER_JAVA8.equals(clazz.getPackageName())) {
