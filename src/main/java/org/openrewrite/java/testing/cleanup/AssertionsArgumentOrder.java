a2c78062db1f1626effab2c9df3cb611f9fb2ee9
Patrick <patway99@gmail.com>
2022-10-26 08:23:24 -0700

    Move AssertionsArgumentOrder to cleanup and add to CleanupAssertions recipe

--- src/main/java/org/openrewrite/java/testing/cleanup/AssertionsArgumentOrder.java
+++ src/main/java/org/openrewrite/java/testing/cleanup/AssertionsArgumentOrder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.testing.assertj;
+package org.openrewrite.java.testing.cleanup;
 
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
