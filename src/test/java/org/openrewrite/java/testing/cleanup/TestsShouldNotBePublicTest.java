d8076d924335adfe52d174e063f7dcdfc8e3793d
Tim te Beek <tim@moderne.io>
2023-02-22 22:48:51 +0100

    Use default void for interface methods with body

--- src/test/java/org/openrewrite/java/testing/cleanup/TestsShouldNotBePublicTest.java
+++ src/test/java/org/openrewrite/java/testing/cleanup/TestsShouldNotBePublicTest.java
@@ -163,47 +163,19 @@ class TestsShouldNotBePublicTest implements RewriteTest {
         rewriteRun(
           java(
             """
-              import org.junit.jupiter.api.AfterEach;
               import org.junit.jupiter.api.BeforeEach;
               import org.junit.jupiter.api.Test;
 
               public interface ATest {
 
                   @BeforeEach
-                  public void beforeEach_public() {
-                  }
-
-                  @BeforeEach
-                  protected void beforeEach_protected() {
-                  }
-
-                  @BeforeEach
-                  void beforeEach_packagePrivate() {
-                  }
-
-                  @AfterEach
-                  public void afterEach_public() {
-                  }
-
-                  @AfterEach
-                  protected void afterEach_protected() {
-                  }
-
-                  @AfterEach
-                  void afterEach_packagePrivate() {
+                  default void beforeEach_public() {
                   }
 
                   @Test
-                  void testMethod_packagePrivate() {
+                  default void testMethod_packagePrivate() {
                   }
 
-                  @Test
-                  public void testMethod_public() {
-                  }
-
-                  @Test
-                  protected void testMethod_protected() {
-                  }
               }
               """
           )
