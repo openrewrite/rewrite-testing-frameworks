7f52192ec26e8f7f3b81d5bb284700259cd39f03
Paolo Bizzarri <pibizza@gmail.com>
2023-05-25 22:16:23 +0200

    Update src/main/java/org/openrewrite/java/testing/assertj/UseExplicitIsEmpty.java
    Co-authored-by: Knut Wannheden <knut.wannheden@gmail.com>

--- src/main/java/org/openrewrite/java/testing/assertj/UseExplicitIsEmpty.java
+++ src/main/java/org/openrewrite/java/testing/assertj/UseExplicitIsEmpty.java
@@ -38,7 +38,7 @@ public class UseExplicitIsEmpty extends Recipe {
 
     @Override
     public String getDescription() {
-        return "Convert AssertJ `assertThat(collection.isEmpty()).isTrue()` with assertThat(collection).isEmpty() "
+        return "Convert AssertJ `assertThat(collection.isEmpty()).isTrue()` to `assertThat(collection).isEmpty()` "
                 + "and `assertThat(collection.isEmpty()).isFalse()` to `assertThat(collection).isNotEmpty()`.";
     }
 
