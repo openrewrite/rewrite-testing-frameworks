83fd44e428be9b97c5404f30d586ba9f0bebbe01
Knut Wannheden <knut@moderne.io>
2023-06-28 18:46:47 +0200

    Fix failing test case due to type attribution validation

--- src/test/java/org/openrewrite/java/testing/junit5/AddParameterizedTestAnnotationTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/AddParameterizedTestAnnotationTest.java
@@ -419,28 +419,48 @@ class AddParameterizedTestAnnotationTest implements RewriteTest {
           //language=java
           java(
             """
+              import org.junit.jupiter.api.extension.ExtensionContext;
+              import org.junit.jupiter.params.provider.Arguments;
+              import org.junit.jupiter.params.provider.ArgumentsProvider;
               import org.junit.jupiter.params.provider.ArgumentsSource;
               import org.junit.jupiter.api.Test;
+              import java.util.stream.Stream;
               import static org.junit.jupiter.api.Assertions.*;
-                           
+              
               class TestClass {
                   @Test
                   @ArgumentsSource(MyArgumentsProvider.class)
                   void testWithArgumentsSource(String argument) {
                       assertNotNull(argument);
                   }
+                  static class MyArgumentsProvider implements ArgumentsProvider {
+                      @Override
+                      public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+                          return Stream.of("apple", "banana").map(Arguments::of);
+                      }
+                  }
               }
               """,
             """
+              import org.junit.jupiter.api.extension.ExtensionContext;
               import org.junit.jupiter.params.ParameterizedTest;
+              import org.junit.jupiter.params.provider.Arguments;
+              import org.junit.jupiter.params.provider.ArgumentsProvider;
               import org.junit.jupiter.params.provider.ArgumentsSource;
+              import java.util.stream.Stream;
               import static org.junit.jupiter.api.Assertions.*;
-                            
+              
               class TestClass {
                   @ParameterizedTest
                   @ArgumentsSource(MyArgumentsProvider.class)
                   void testWithArgumentsSource(String argument) {
                       assertNotNull(argument);
+                  }
+                  static class MyArgumentsProvider implements ArgumentsProvider {
+                      @Override
+                      public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+                          return Stream.of("apple", "banana").map(Arguments::of);
+                      }
                   }
               }
               """
