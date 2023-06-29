8b8ada92ac599b6ca4008049efcfdf5daeac8dca
AlekSimpson <alek@asu.me>
2023-06-29 15:58:27 -0700

    updated license header and added test to cover negative case

--- src/test/java/org/openrewrite/java/testing/junit5/RemoveTryCatchBlocksFromUnitTestsTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/RemoveTryCatchBlocksFromUnitTestsTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.junit5;
 
 import org.junit.jupiter.api.Test;
@@ -46,6 +61,28 @@ public class RemoveTryCatchBlocksFromUnitTestsTest implements RewriteTest {
                 @Test
                 public void testMethod() throws ArithmeticException {
                   int divide = 50/0;
+                }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void onlyAffectsUnitTests() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              import static org.junit.Assert.fail;
+              
+              class Test {
+                public void method() {
+                  try {
+                    int divide = 50/0;
+                  }catch (ArithmeticException e) {
+                    Assert.fail(e.getMessage());
+                  }
                 }
               }
               """
