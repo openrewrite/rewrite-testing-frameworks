5006136af6f597f1c37cb043c7b5788897488556
Tim te Beek <tim@moderne.io>
2023-07-20 11:07:26 +0200

    Wrap statements in a block and remove the block after apply

--- src/main/java/org/openrewrite/java/testing/hamcrest/FlattenAllOf.java
+++ src/main/java/org/openrewrite/java/testing/hamcrest/FlattenAllOf.java
@@ -1,6 +1,9 @@
 package org.openrewrite.java.testing.hamcrest;
 
-import org.openrewrite.*;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
@@ -8,13 +11,10 @@ import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.JavaCoordinates;
-import org.openrewrite.java.tree.Statement;
+import org.openrewrite.staticanalysis.RemoveUnneededBlock;
 
-import javax.naming.ldap.ExtendedRequest;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FlattenAllOf extends Recipe {
     @Override
@@ -40,68 +40,44 @@ class FlattenAllOfVisitor extends JavaVisitor<ExecutionContext> {
     private final MethodMatcher ASSERT_THAT_MATCHER = new MethodMatcher("org.hamcrest.MatcherAssert assertThat(..)");
     private final MethodMatcher ALL_OF_MATCHER = new MethodMatcher("org.hamcrest.Matchers allOf(..)");
 
-    //@Override
-    //public J visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-    //    J.MethodDeclaration md = (J.MethodDeclaration) super.visitMethodDeclaration(method, ctx);
-    //    if (md.getBody() == null) { return md; }
-
-    //    List<Statement> newStatements = md.getBody().getStatements().stream().map(statement -> {
-    //        if (statement instanceof J.MethodInvocation) {
-    //            J.MethodInvocation assertThat = (J.MethodInvocation) statement;
-    //            if (ASSERT_THAT_MATCHER.matches(assertThat)) {
-    //                List<Expression> assertThatArguments = assertThat.getArguments();
-    //                Expression actual = assertThatArguments.get(assertThatArguments.size() - 2);
-    //                Expression matcherExpression = assertThatArguments.get(assertThatArguments.size() - 1);
-    //                if (ALL_OF_MATCHER.matches(matcherExpression)) {
-    //                    J.MethodInvocation allOf = (J.MethodInvocation) matcherExpression;
-    //                    List<Expression> matchers = allOf.getArguments();
-    //                    for (Expression matcher : matchers) {
-    //                        JavaTemplate template = JavaTemplate.builder("assertThat(#{any(java.lang.Object)}, #{any(org.hamcrest.Matcher)});")
-    //                                .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "hamcrest-2.2"))
-    //                                .staticImports("org.hamcrest.MatcherAssert.assertThat")
-    //                                .build();
-    //                        // TODO figure out correct use of coordinates and cursor when inserting multiple new statements
-    //                        JavaCoordinates coordinates = assertThat.getCoordinates().replace();
-    //                        Cursor cursor = null;
-    //                        return template.apply(getCursor(), coordinates, actual, matcher);
-    //                    }
-    //                    //continue;
-    //                }
-    //            }
-    //        }
-    //        // Keep the original statement
-    //        return statement;
-    //    }).collect(Collectors.toList());
-
-
-    //    return md.withBody(md.getBody().withStatements(newStatements));
-    //}
-
     @Override
     public J visitMethodInvocation(J.MethodInvocation invocation, ExecutionContext ctx) {
         J.MethodInvocation mi = (J.MethodInvocation) super.visitMethodInvocation(invocation, ctx);
 
         List<Expression> arguments = mi.getArguments();
-        if (!ASSERT_THAT_MATCHER.matches(mi) && !ALL_OF_MATCHER.matches(arguments.get(arguments.size() - 1))) {
+        Expression allOf = arguments.get(arguments.size() - 1);
+        if (!ASSERT_THAT_MATCHER.matches(mi) && !ALL_OF_MATCHER.matches(allOf)) {
             return mi;
         }
 
+        Expression reason = arguments.size() == 3 ? arguments.get(0) : null;
         Expression actual = arguments.get(arguments.size() - 2);
-        List<Expression> allOfArgs = ((J.MethodInvocation) arguments.get(arguments.size() - 1)).getArguments();
 
-        StringBuilder stringBuilderTemplate = new StringBuilder();
-        List<Expression> replacements = new ArrayList<>();
-        for (Expression matcher : allOfArgs) {
-            stringBuilderTemplate.append("assertThat(#{any(java.lang.Object)}, #{any(org.hamcrest.Matcher)});\n");
-            replacements.add(actual);
-            replacements.add(matcher);
+        // Wrap statements in a block, as JavaTemplate can only return one element
+        StringBuilder blockTemplate = new StringBuilder();
+        blockTemplate.append("{");
+        List<Expression> parameters = new ArrayList<>();
+        for (Expression matcher : ((J.MethodInvocation) allOf).getArguments()) {
+            if (reason == null) {
+                blockTemplate.append("assertThat(#{any(java.lang.Object)}, #{any(org.hamcrest.Matcher)});");
+            } else {
+                blockTemplate.append("assertThat(#{any(java.lang.String)}, #{any(java.lang.Object)}, #{any(org.hamcrest.Matcher)});");
+                parameters.add(reason);
+            }
+            parameters.add(actual);
+            parameters.add(matcher);
         }
-        JavaTemplate template = JavaTemplate.builder(stringBuilderTemplate.toString())
+        blockTemplate.append("}");
+
+        JavaTemplate template = JavaTemplate.builder(blockTemplate.toString())
                 .contextSensitive()
                 .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "hamcrest-2.2"))
                 .staticImports("org.hamcrest.MatcherAssert.assertThat")
                 .build();
 
-        return template.apply(getCursor(), mi.getCoordinates().replace(), replacements.toArray());
+        // Remove wrapping block and allOf import after template is applied
+        doAfterVisit(new RemoveUnneededBlock().getVisitor());
+        maybeRemoveImport("org.hamcrest.Matchers.allOf");
+        return template.apply(getCursor(), mi.getCoordinates().replace(), parameters.toArray());
     }
 }
