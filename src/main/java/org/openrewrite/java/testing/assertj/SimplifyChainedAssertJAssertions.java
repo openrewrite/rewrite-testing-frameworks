fc63d1d0486dd4d779861ad955a8be17689d5692
AlekSimpson <alek@asu.me>
2023-07-23 11:30:40 -0700

    working draft, need to update yaml and add new test file for full list of test cases

--- src/main/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertions.java
+++ src/main/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertions.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.assertj;
 
 import lombok.AllArgsConstructor;
@@ -12,6 +27,11 @@ import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @AllArgsConstructor
 public class SimplifyChainedAssertJAssertions extends Recipe {
     @Option(displayName = "AssertJ Assertion",
@@ -21,9 +41,9 @@ public class SimplifyChainedAssertJAssertions extends Recipe {
     @Nullable
     String chainedAssertion;
 
-    @Option(displayName = "",
-            description = "",
-            example = "",
+    @Option(displayName = "AssertJ Assertion",
+            description = "The AssertJ assert that should be replaced",
+            example = "isTrue",
             required = false)
     @Nullable
     String assertToReplace;
@@ -43,7 +63,7 @@ public class SimplifyChainedAssertJAssertions extends Recipe {
     @Override
     public String getDescription() {
         return "Many AssertJ chained assertions have dedicated assertions that function the same. " +
-               "It is best to use the dedicated assertions.";
+                "It is best to use the dedicated assertions.";
     }
 
     @Override
@@ -54,36 +74,60 @@ public class SimplifyChainedAssertJAssertions extends Recipe {
     private class SimplifyChainedAssertJAssertionsVisitor extends JavaIsoVisitor<ExecutionContext> {
         private final MethodMatcher ASSERT_THAT_MATCHER = new MethodMatcher("org.junit.jupiter.api.Assertions assertThat(..)");
         private final MethodMatcher CHAINED_ASSERT_MATCHER = new MethodMatcher("java..* " + chainedAssertion + "(..)");
-        private final MethodMatcher ASSERT_TO_REPLACE = new MethodMatcher("org.assertj.core.api.AbstractAssert " + assertToReplace + "()");
+        private final MethodMatcher ASSERT_TO_REPLACE = new MethodMatcher("org.assertj.core.api.*" + assertToReplace + "(..)");
 
         @Override
         public J.MethodInvocation visitMethodInvocation(J.MethodInvocation mi, ExecutionContext ctx) {
             //  assert has assertion
-            if (!ASSERT_TO_REPLACE.matches(mi)) {
+            if (!ASSERT_TO_REPLACE.matches(mi) || mi.getSelect() == null) {
                 return mi;
             }
 
-            J.MethodInvocation dedicatedSelect = (J.MethodInvocation)mi.getSelect();
             //assertThat has method call
-            if (!ASSERT_THAT_MATCHER.matches(dedicatedSelect) && !(dedicatedSelect.getArguments().get(0) instanceof J.MethodInvocation)) {
+            J.MethodInvocation assertThat = (J.MethodInvocation)mi.getSelect();
+            if (!ASSERT_THAT_MATCHER.matches(assertThat) && !(assertThat.getArguments().get(0) instanceof J.MethodInvocation)) {
                 return mi;
             }
 
-            J.MethodInvocation firstArgument = (J.MethodInvocation) dedicatedSelect.getArguments().get(0);
-            Expression select = firstArgument;
-            //  method call has select
-            if (firstArgument.getSelect() != null) {
-                select = firstArgument.getSelect();
+            J.MethodInvocation assertThatArg = (J.MethodInvocation)assertThat.getArguments().get(0);
+            if (!CHAINED_ASSERT_MATCHER.matches(assertThatArg) && assertThat.getSelect() != null) {
+                return mi;
             }
 
-            String stringTemplate = mi.getArguments().size() == 0
-                    ? String.format("assertThat(%s).%s(%s)", select, dedicatedAssertion, mi.getArguments().get(0))
-                    : String.format("assertThat(#{any()}).%s()", dedicatedAssertion);
+            //  method call has select
+            Expression select = assertThatArg.getSelect() != null ? assertThatArg.getSelect() : assertThatArg;
 
-            return JavaTemplate.builder(stringTemplate)
+            List<Expression> arguments = new ArrayList<>(Collections.singletonList(select));
+            String template = getTemplate(arguments, assertThatArg, mi);
+            String formattedTemplate = String.format(template, dedicatedAssertion);
+
+            return JavaTemplate.builder(formattedTemplate)
+                    .contextSensitive()
                     .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "junit-jupiter-api-5.9", "assertj-core-3.24"))
                     .build()
-                    .apply(getCursor(), mi.getCoordinates().replace(), select);
+                    .apply(getCursor(), mi.getCoordinates().replace(), arguments.toArray());
         }
+    }
+
+    private String getTemplate(List<Expression> arguments, J.MethodInvocation assertThatArg, J.MethodInvocation methodToReplace) {
+        String template = "assertThat(#{any()}).%s()";
+        if (methodToReplace.getArguments().get(0) instanceof J.Literal) {
+            J.Literal literalArg = (J.Literal) methodToReplace.getArguments().get(0);
+            if (literalArg.getValue() != null && literalArg.getValue().equals(0)) {
+                return template;
+            }
+        }
+
+        if (!(assertThatArg.getArguments().get(0) instanceof J.Empty) && !(methodToReplace.getArguments().get(0) instanceof J.Empty)) {
+            // Note: this should be the only case when more than one argument needs to be handled. When the assertions involve the map functions
+            arguments.add(assertThatArg.getArguments().get(0));
+            arguments.add(methodToReplace.getArguments().get(0));
+            template = "assertThat(#{any()}).%s(#{any()}, #{any()})";
+        }else if (!(assertThatArg.getArguments().get(0) instanceof J.Empty) || !(methodToReplace.getArguments().get(0) instanceof J.Empty)) {
+            arguments.add( assertThatArg.getArguments().get(0) instanceof J.Empty ? methodToReplace.getArguments().get(0) : assertThatArg.getArguments().get(0) );
+            template = "assertThat(#{any()}).%s(#{any()})";
+        }
+
+        return template;
     }
 }
--- src/test/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertionsTest.java
+++ src/test/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertionsTest.java
@@ -32,6 +32,10 @@ public class SimplifyChainedAssertJAssertionsTest implements RewriteTest {
                   void testMethod() {
                       assertThat(getString().isEmpty()).isTrue();
                   }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
               }
               """,
             """
@@ -43,6 +47,233 @@ public class SimplifyChainedAssertJAssertionsTest implements RewriteTest {
                   @Test
                   void testMethod() {
                       assertThat(getString()).isEmpty();
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void replacementHasZeroArgument() {
+        rewriteRun(
+          spec -> spec.recipe(new SimplifyChainedAssertJAssertions("getString", "hasSize", "isEmpty")),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      assertThat(getString()).hasSize(0);
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      assertThat(getString()).isEmpty();
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void assertThatArgHasArgument() {
+        rewriteRun(
+          spec -> spec.recipe(new SimplifyChainedAssertJAssertions("equalsIgnoreCase", "isTrue", "isEqualToIgnoringCase")),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      String expected = "hello world";
+                      assertThat(getString().equalsIgnoreCase(expected)).isTrue();
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      String expected = "hello world";
+                      assertThat(getString()).isEqualToIgnoringCase(expected);
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void replacementHasArgument() {
+        rewriteRun(
+          spec -> spec.recipe(new SimplifyChainedAssertJAssertions("length", "isEqualTo", "hasSize")),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      int length = 5;
+                      assertThat(getString().length()).isEqualTo(length);
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      int length = 5;
+                      assertThat(getString()).hasSize(length);
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """
+          )
+        );
+
+    }
+
+    @Test
+    void normalCase() {
+        rewriteRun(
+          spec -> spec.recipe(new SimplifyChainedAssertJAssertions("trim", "isEmpty", "isBlank")),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      assertThat(getString().trim()).isEmpty();
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      assertThat(getString()).isBlank();
+                  }
+                  
+                  String getString() {
+                      return "hello world";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void mapMethodDealsWithTwoArguments() {
+        rewriteRun(
+          spec -> spec.recipe(new SimplifyChainedAssertJAssertions("get", "isEqualTo", "containsEntry")),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              import java.util.Collections;
+              import java.util.Map;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      String key = "key";
+                      String value = "value";
+                      assertThat(getMap().get(key)).isEqualTo(value);
+                  }
+                  
+                  Map<String, String> getMap() {
+                      return Collections.emptyMap();
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              import java.util.Collections;
+              import java.util.Map;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+              
+              class MyTest {
+                  @Test
+                  void testMethod() {
+                      String key = "key";
+                      String value = "value";
+                      assertThat(getMap()).containsEntry(key, value);
+                  }
+                  
+                  Map<String, String> getMap() {
+                      return Collections.emptyMap();
                   }
               }
               """
