595a2731052aa03e67ae4d6602fb1b42fa3c9ae9
Simon Zilliken <45996518+simonzn@users.noreply.github.com>
2023-11-22 20:24:26 +0100

    Fixes broken testcontainer dependencies after JUnit4to5Migration (#430)
    * Adds test case for broken testcontainers dependency
    
    Reproduces #429
    
    * Remove JUnit4 exclusion from testcontainers dependency
    
    Fixes #429
    
    * Document why output is identical but still different
    
    ---------
    
    Co-authored-by: e512271 <simon.zilliken@sbb.ch>
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
@@ -27,7 +27,6 @@ import static org.openrewrite.java.Assertions.java;
 import static org.openrewrite.maven.Assertions.pomXml;
 
 class JUnit5MigrationTest implements RewriteTest {
-
     @Override
     public void defaults(RecipeSpec spec) {
         spec
@@ -43,25 +42,26 @@ class JUnit5MigrationTest implements RewriteTest {
     @Test
     void classReference() {
         rewriteRun(
+          //language=java
           java(
             """
-            import org.junit.Test;
-            
-            public class Sample {
-                void method() {
-                    Class<Test> c = Test.class;
-                }
-            }
-            """,
+              import org.junit.Test;
+                          
+              public class Sample {
+                  void method() {
+                      Class<Test> c = Test.class;
+                  }
+              }
+              """,
             """
-            import org.junit.jupiter.api.Test;
-            
-            public class Sample {
-                void method() {
-                    Class<Test> c = Test.class;
-                }
-            }
-            """
+              import org.junit.jupiter.api.Test;
+                          
+              public class Sample {
+                  void method() {
+                      Class<Test> c = Test.class;
+                  }
+              }
+              """
           )
         );
     }
@@ -112,53 +112,78 @@ class JUnit5MigrationTest implements RewriteTest {
     @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/279")
     void upgradeMavenPluginVersions() {
         rewriteRun(
+          //language=xml
           pomXml(
             """
-            <project>
-                <modelVersion>4.0.0</modelVersion>
-                <groupId>com.example.jackson</groupId>
-                <artifactId>test-plugins</artifactId>
-                <version>1.0.0</version>
-                <build>
-                    <plugins>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-surefire-plugin</artifactId>
-                            <version>2.20.1</version>
-                        </plugin>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-failsafe-plugin</artifactId>
-                            <version>2.20.1</version>
-                        </plugin>
-                    </plugins>
-                </build>
-            </project>
-            """,
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.example.jackson</groupId>
+                  <artifactId>test-plugins</artifactId>
+                  <version>1.0.0</version>
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-surefire-plugin</artifactId>
+                              <version>2.20.1</version>
+                          </plugin>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-failsafe-plugin</artifactId>
+                              <version>2.20.1</version>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """,
             """
-            <project>
-                <modelVersion>4.0.0</modelVersion>
-                <groupId>com.example.jackson</groupId>
-                <artifactId>test-plugins</artifactId>
-                <version>1.0.0</version>
-                <build>
-                    <plugins>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-surefire-plugin</artifactId>
-                            <version>2.22.2</version>
-                        </plugin>
-                        <plugin>
-                            <groupId>org.apache.maven.plugins</groupId>
-                            <artifactId>maven-failsafe-plugin</artifactId>
-                            <version>2.22.2</version>
-                        </plugin>
-                    </plugins>
-                </build>
-            </project>
-            """
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.example.jackson</groupId>
+                  <artifactId>test-plugins</artifactId>
+                  <version>1.0.0</version>
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-surefire-plugin</artifactId>
+                              <version>2.22.2</version>
+                          </plugin>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-failsafe-plugin</artifactId>
+                              <version>2.22.2</version>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """
           )
         );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/429")
+    void dontExcludeJunit4DependencyfromTestcontainers() {
+        //language=xml
+        String before = """
+          <project>
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>com.example.jackson</groupId>
+              <artifactId>test-plugins</artifactId>
+              <version>1.0.0</version>
+              <dependencies>
+                  <dependency>
+                      <groupId>org.testcontainers</groupId>
+                      <artifactId>testcontainers</artifactId>
+                      <version>1.18.3</version>
+                      <scope>test</scope>
+                  </dependency>
+              </dependencies>
+          </project>
+          """;
+        // Output identical, but we want to make sure we don't exclude junit4 from testcontainers
+        rewriteRun(pomXml(before, before));
     }
 
     // edge case for deprecated use of assertEquals
@@ -166,12 +191,12 @@ class JUnit5MigrationTest implements RewriteTest {
     @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/pull/384")
     @Test
     void assertEqualsWithArrayArgumentToAssertArrayEquals() {
-        //language=java
         rewriteRun(
+          //language=java
           java(
             """
               import org.junit.Assert;
-              
+                            
               class MyTest {
                   void test() {
                        Assert.assertEquals(new Object[1], new Object[1]);
@@ -180,7 +205,7 @@ class JUnit5MigrationTest implements RewriteTest {
               """,
             """
               import org.junit.jupiter.api.Assertions;
-              
+                            
               class MyTest {
                   void test() {
                        Assertions.assertArrayEquals(new Object[1], new Object[1]);
