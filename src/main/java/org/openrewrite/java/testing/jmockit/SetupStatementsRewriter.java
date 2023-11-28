6e56c0540a86106afd29c43f25938e47080988c3
tinder-dthomson <devin.thomson@gotinder.com>
2023-11-28 13:54:47 -0800

    polish

--- src/main/java/org/openrewrite/java/testing/jmockit/SetupStatementsRewriter.java
+++ src/main/java/org/openrewrite/java/testing/jmockit/SetupStatementsRewriter.java
@@ -1,5 +1,6 @@
 package org.openrewrite.java.testing.jmockit;
 
+import org.jetbrains.annotations.Nullable;
 import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaParser;
@@ -27,7 +28,7 @@ class SetupStatementsRewriter {
         J.Block newBody = methodBody;
         try {
             // iterate over each statement in the method body, find Expectations blocks and rewrite them
-            for (Statement s : newBody.getStatements()) {
+            for (Statement s : methodBody.getStatements()) {
                 if (!JMockitUtils.isExpectationsNewClassStatement(s)) {
                     continue;
                 }
@@ -36,54 +37,29 @@ class SetupStatementsRewriter {
                 assert nc.getBody() != null;
                 J.Block expectationsBlock = (J.Block) nc.getBody().getStatements().get(0);
                 for (Statement expectationStatement : expectationsBlock.getStatements()) {
-                    boolean isSetupStatement = true;
-                    if (expectationStatement instanceof J.MethodInvocation) {
-                        // a method invocation on a mock is not a setup statement
-                        J.MethodInvocation methodInvocation = (J.MethodInvocation) expectationStatement;
-                        if (methodInvocation.getSelect() instanceof J.Identifier) {
-                            J.Identifier identifier = (J.Identifier) methodInvocation.getSelect();
-                            JavaType.Variable fieldType = identifier.getFieldType();
-                            if (fieldType == null) {
-                                continue;
-                            }
-                            for (JavaType.FullyQualified annotationType : fieldType.getAnnotations()) {
-                                if (TypeUtils.isAssignableTo("mockit.Mocked", annotationType)
-                                        || TypeUtils.isAssignableTo("org.mockito.Mock", annotationType)) {
-                                    isSetupStatement = false;
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (expectationStatement instanceof J.Assignment) {
-                        // an assignment to a jmockit reserved field is not a setup statement
-                        J.Assignment assignment = (J.Assignment) expectationStatement;
-                        J.Identifier identifier = (J.Identifier) assignment.getVariable();
-                        if (identifier.getSimpleName().equals("result") || identifier.getSimpleName().equals("times")) {
-                            isSetupStatement = false;
-                        }
+                    if (!isSetupStatement(expectationStatement)) {
+                        continue;
                     }
-                    if (isSetupStatement) {
-                        // statement needs to be moved directly before expectations class instantiation
-                        newBody = copySetupStatement(expectationStatement, newBody, nc);
+                    // statement needs to be moved directly before expectations class instantiation
+                    newBody = copySetupStatement(expectationStatement, newBody, nc);
 
-                        // the expectations block needs to have the statement removed
-                        List<Statement> newExpectationsBlockStatements = new ArrayList<>(expectationsBlock.getStatements());
-                        newExpectationsBlockStatements.remove(expectationStatement);
-                        J.Block newExpectationsBlock = expectationsBlock.withStatements(newExpectationsBlockStatements);
-                        List<Statement> newExpectationsStatements = new ArrayList<>();
-                        newExpectationsStatements.add(newExpectationsBlock);
-                        assert nc.getBody() != null;
-                        nc = nc.withBody(nc.getBody().withStatements(newExpectationsStatements));
+                    // the expectations block needs to have the statement removed
+                    List<Statement> newExpectationsBlockStatements = new ArrayList<>(expectationsBlock.getStatements());
+                    newExpectationsBlockStatements.remove(expectationStatement);
+                    J.Block newExpectationsBlock = expectationsBlock.withStatements(newExpectationsBlockStatements);
+                    List<Statement> newExpectationsStatements = new ArrayList<>();
+                    newExpectationsStatements.add(newExpectationsBlock);
+                    assert nc.getBody() != null;
+                    nc = nc.withBody(nc.getBody().withStatements(newExpectationsStatements));
 
-                        newBody = JavaTemplate.builder("#{any()}")
-                                .javaParser(JavaParser.fromJavaVersion())
-                                .build()
-                                .apply(
-                                        new Cursor(visitor.getCursor(), newBody),
-                                        nc.getCoordinates().replace(),
-                                        nc
-                                );
-                    }
+                    newBody = JavaTemplate.builder("#{any()}")
+                            .javaParser(JavaParser.fromJavaVersion())
+                            .build()
+                            .apply(
+                                    new Cursor(visitor.getCursor(), newBody),
+                                    nc.getCoordinates().replace(),
+                                    nc
+                            );
                 }
             }
         }  catch (Exception e) {
@@ -91,6 +67,38 @@ class SetupStatementsRewriter {
             return methodBody;
         }
         return newBody;
+    }
+
+    private static boolean isSetupStatement(Statement expectationStatement) {
+        boolean isSetupStatement = true;
+        if (expectationStatement instanceof J.MethodInvocation) {
+            // a method invocation on a mock is not a setup statement
+            J.MethodInvocation methodInvocation = (J.MethodInvocation) expectationStatement;
+            if (methodInvocation.getSelect() instanceof J.Identifier) {
+                J.Identifier identifier = (J.Identifier) methodInvocation.getSelect();
+                JavaType.Variable fieldType = identifier.getFieldType();
+
+                // if type information is missing, don't move the statement
+                if (fieldType == null) {
+                    return false;
+                }
+                for (JavaType.FullyQualified annotationType : fieldType.getAnnotations()) {
+                    if (TypeUtils.isAssignableTo("mockit.Mocked", annotationType)
+                            || TypeUtils.isAssignableTo("org.mockito.Mock", annotationType)) {
+                        isSetupStatement = false;
+                        break;
+                    }
+                }
+            }
+        } else if (expectationStatement instanceof J.Assignment) {
+            // an assignment to a jmockit reserved field is not a setup statement
+            J.Assignment assignment = (J.Assignment) expectationStatement;
+            J.Identifier identifier = (J.Identifier) assignment.getVariable();
+            if (identifier.getSimpleName().equals("result") || identifier.getSimpleName().equals("times")) {
+                isSetupStatement = false;
+            }
+        }
+        return isSetupStatement;
     }
 
     private J.Block copySetupStatement(Statement setupStatement, J.Block newBody, J.NewClass expectationsClass) {
