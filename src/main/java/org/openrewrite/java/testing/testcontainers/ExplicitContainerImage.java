38e174bee556f7b5a2fa0692c10dbf4a5b8c26c5
Tim te Beek <tim@moderne.io>
2024-01-19 18:10:02 +0100

    Apply suggestions from code review

--- src/main/java/org/openrewrite/java/testing/testcontainers/ExplicitContainerImage.java
+++ src/main/java/org/openrewrite/java/testing/testcontainers/ExplicitContainerImage.java
@@ -67,7 +67,6 @@ public class ExplicitContainerImage extends Recipe {
             @Override
             public J.NewClass visitNewClass(J.NewClass newClass, ExecutionContext ctx) {
                 J.NewClass nc = super.visitNewClass(newClass, ctx);
-
                 if (methodMatcher.matches(nc)) {
                     Expression constructorArgument = getConstructorArgument(nc);
                     return nc.withArguments(Arrays.asList(constructorArgument))
