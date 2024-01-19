02d9f2bcd30912cbf835e1646c238b9cd40f4f17
Tim te Beek <tim@moderne.io>
2024-01-19 18:20:03 +0100

    Apply suggestions from code review

--- src/main/java/org/openrewrite/java/testing/testcontainers/ExplicitContainerImage.java
+++ src/main/java/org/openrewrite/java/testing/testcontainers/ExplicitContainerImage.java
@@ -29,7 +29,6 @@ import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.Space;
 import org.openrewrite.marker.Markers;
 
-import java.util.Collections;
 
 import static java.util.Collections.singletonList;
 
