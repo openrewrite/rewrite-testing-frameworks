e6c7d0e71c74b07fe5fccb80f86e0c5bd3a39f93
timo <129042237+timo-abele@users.noreply.github.com>
2024-01-25 19:39:00 +0100

    add dependencies copied from rewrite-recipe-starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,4 +52,20 @@ dependencies {
 
 //    testImplementation("org.hamcrest:hamcrest:latest.release")
 //    testImplementation("org.assertj:assertj-core:latest.release")
+
+    // The bom version can also be set to a specific version
+    // https://github.com/openrewrite/rewrite-recipe-bom/releases
+    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:latest.release"))
+
+    // Refaster style recipes need the rewrite-templating annotation processor and dependency for generated recipes
+    // https://github.com/openrewrite/rewrite-templating/releases
+    annotationProcessor("org.openrewrite:rewrite-templating:latest.release")
+    implementation("org.openrewrite:rewrite-templating")
+    // The `@BeforeTemplate` and `@AfterTemplate` annotations are needed for refaster style recipes
+    compileOnly("com.google.errorprone:error_prone_core:2.19.1") {
+        exclude("com.google.auto.service", "auto-service-annotations")
+    }
+
+    implementation("org.assertj:assertj-core:3.+")
+
 }
