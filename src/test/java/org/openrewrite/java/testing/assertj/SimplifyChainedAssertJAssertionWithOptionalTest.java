d06a0f3f31db27b763edbe7593c2a34559a66921
Tim te Beek <timtebeek@gmail.com>
2024-02-06 21:59:48 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertionWithOptionalTest.java
+++ src/test/java/org/openrewrite/java/testing/assertj/SimplifyChainedAssertJAssertionWithOptionalTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.testing.assertj;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
@@ -32,6 +33,7 @@ class SimplifyChainedAssertJAssertionWithOptionalTest implements RewriteTest {
     }
 
 
+    @DocumentExample
     @Test
     void simplifyPresenceAssertion() {
         rewriteRun(
