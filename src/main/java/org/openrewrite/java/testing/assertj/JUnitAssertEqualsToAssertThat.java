openrewrite/rewrite-testing-frameworks

commit 368384a174bd92d49880c19a879fb9e14afcfa27

Use context sensitive templates where argument can be custom
Fixes https://github.com/openrewrite/rewrite-testing-frameworks/issues/491

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertEqualsToAssertThat.java
@@ -67,6 +67,7 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
 
             if (args.size() == 2) {
                 return JavaTemplate.builder("assertThat(#{any()}).isEqualTo(#{any()});")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
                         .build()
@@ -77,6 +78,7 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(String)}).isEqualTo(#{any()});") :
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isEqualTo(#{any()});");
                 return template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .imports("java.util.function.Supplier")
                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
@@ -91,6 +93,7 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
             } else if (args.size() == 3) {
                 maybeAddImport("org.assertj.core.api.Assertions", "within");
                 return JavaTemplate.builder("assertThat(#{any()}).isCloseTo(#{any()}, within(#{any()}));")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
                         .build()
@@ -106,6 +109,7 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
                     JavaTemplate.builder("assertThat(#{any()}).as(#{any(String)}).isCloseTo(#{any()}, within(#{any()}));") :
                     JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isCloseTo(#{any()}, within(#{any()}));");
             return template
+                    .contextSensitive()
                     .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                     .imports("java.util.function.Supplier")
                     .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNotEqualsToAssertThat.java
@@ -74,6 +74,7 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
 
             if (args.size() == 2) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotEqualTo(#{any()});")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -92,6 +93,7 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
 
 
                 method = template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -104,6 +106,7 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                         );
             } else if (args.size() == 3) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotCloseTo(#{any()}, within(#{any()}));")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -123,6 +126,7 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNotCloseTo(#{any()}, within(#{any()}));");
 
                 method = template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                         .javaParser(assertionsParser(ctx))
                         .build()

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNotNullToAssertThat.java
@@ -71,6 +71,7 @@ public class JUnitAssertNotNullToAssertThat extends Recipe {
 
             if (args.size() == 1) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotNull();")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -88,6 +89,7 @@ public class JUnitAssertNotNullToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNotNull();");
 
                 method = template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNullToAssertThat.java
@@ -71,6 +71,7 @@ public class JUnitAssertNullToAssertThat extends Recipe {
 
             if (args.size() == 1) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNull();")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -87,6 +88,7 @@ public class JUnitAssertNullToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNull();");
 
                 method = template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertSameToAssertThat.java
@@ -72,6 +72,7 @@ public class JUnitAssertSameToAssertThat extends Recipe {
 
             if (args.size() == 2) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isSameAs(#{any()});")
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -89,6 +90,7 @@ public class JUnitAssertSameToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isSameAs(#{any()});");
 
                 method = template
+                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()

src/test/java/org/openrewrite/java/testing/assertj/JUnitAssertNotNullToAssertThatTest.java
@@ -18,6 +18,7 @@ package org.openrewrite.java.testing.assertj;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -235,6 +236,46 @@ class JUnitAssertNotNullToAssertThatTest implements RewriteTest {
                   }
                   private String notification() {
                       return "";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/491")
+    void importAddedForCustomArguments() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+              class TTest {
+
+                  class A {}
+
+                  @Test
+                  public void testClass() {
+                      assertNotNull(new A());
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Test;
+              
+              import static org.assertj.core.api.Assertions.assertThat;
+
+              class TTest {
+
+                  class A {}
+
+                  @Test
+                  public void testClass() {
+                      assertThat(new A()).isNotNull();
                   }
               }
               """

commit a9926973259d35cb4c10608ec78a6a354f253159

Fix performance regression in AssertJ recipes
Since commit https://github.com/openrewrite/rewrite-testing-frameworks/commit/368384a174bd92d49880c19a879fb9e14afcfa27 , AssertJ recipes are really slower since calling contextSensitive() method  disable a cache on JavaTemplate.

Fixes:
- Make sure there is a static import org.assertj.core.api.Assertions.* , even if not referenced (see #491 and #479)
- reintroduced assertionsParser cache in JUnitAssertEqualsToAssertThat

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertArrayEqualsToAssertThat.java
@@ -73,7 +73,8 @@ public class JUnitAssertArrayEqualsToAssertThat extends Recipe {
             Expression expected = args.get(0);
             Expression actual = args.get(1);
 
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
+            // Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
             maybeRemoveImport(JUNIT_QUALIFIED_ASSERTIONS_CLASS_NAME);
 
             if (args.size() == 2) {
@@ -93,7 +94,7 @@ public class JUnitAssertArrayEqualsToAssertThat extends Recipe {
                         .build()
                         .apply(getCursor(), method.getCoordinates().replace(), actual, message, expected);
             } else if (args.size() == 3) {
-                maybeAddImport("org.assertj.core.api.Assertions", "within");
+                maybeAddImport("org.assertj.core.api.Assertions", "within", false);
                 // assert is using floating points with a delta and no message.
                 return JavaTemplate.builder("assertThat(#{anyArray()}).containsExactly(#{anyArray()}, within(#{any()}));")
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
@@ -104,7 +105,7 @@ public class JUnitAssertArrayEqualsToAssertThat extends Recipe {
 
             // The assertEquals is using a floating point with a delta argument and a message.
             Expression message = args.get(3);
-            maybeAddImport("org.assertj.core.api.Assertions", "within");
+            maybeAddImport("org.assertj.core.api.Assertions", "within", false);
 
             JavaTemplate.Builder template = TypeUtils.isString(message.getType()) ?
                     JavaTemplate.builder("assertThat(#{anyArray()}).as(#{any(String)}).containsExactly(#{anyArray()}, within(#{any()}));") :

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertEqualsToAssertThat.java
@@ -49,6 +49,16 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
     }
 
     public static class AssertEqualsToAssertThatVisitor extends JavaIsoVisitor<ExecutionContext> {
+        private JavaParser.Builder<?, ?> assertionsParser;
+
+        private JavaParser.Builder<?, ?> assertionsParser(ExecutionContext ctx) {
+            if (assertionsParser == null) {
+                assertionsParser = JavaParser.fromJavaVersion()
+                        .classpathFromResources(ctx, "assertj-core-3.24");
+            }
+            return assertionsParser;
+        }
+
         private static final MethodMatcher JUNIT_ASSERT_EQUALS = new MethodMatcher("org.junit.jupiter.api.Assertions" + " assertEquals(..)");
 
         @Override
@@ -63,13 +73,14 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
 
             //always add the import (even if not referenced)
             maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
+            // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
 
             if (args.size() == 2) {
                 return JavaTemplate.builder("assertThat(#{any()}).isEqualTo(#{any()});")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
-                        .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
+                        .javaParser(assertionsParser(ctx))
                         .build()
                         .apply(getCursor(), method.getCoordinates().replace(), actual, expected);
             } else if (args.size() == 3 && !isFloatingPointType(args.get(2))) {
@@ -78,10 +89,9 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(String)}).isEqualTo(#{any()});") :
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isEqualTo(#{any()});");
                 return template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .imports("java.util.function.Supplier")
-                        .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
+                        .javaParser(assertionsParser(ctx))
                         .build()
                         .apply(
                                 getCursor(),
@@ -91,11 +101,11 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
                                 expected
                         );
             } else if (args.size() == 3) {
-                maybeAddImport("org.assertj.core.api.Assertions", "within");
+                //always add the import (even if not referenced)
+                maybeAddImport("org.assertj.core.api.Assertions", "within", false);
                 return JavaTemplate.builder("assertThat(#{any()}).isCloseTo(#{any()}, within(#{any()}));")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
-                        .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
+                        .javaParser(assertionsParser(ctx))
                         .build()
                         .apply(getCursor(), method.getCoordinates().replace(), actual, expected, args.get(2));
 
@@ -104,15 +114,15 @@ public class JUnitAssertEqualsToAssertThat extends Recipe {
             // The assertEquals is using a floating point with a delta argument and a message.
             Expression message = args.get(3);
 
-            maybeAddImport("org.assertj.core.api.Assertions", "within");
+            //always add the import (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "within", false);
             JavaTemplate.Builder template = TypeUtils.isString(message.getType()) ?
                     JavaTemplate.builder("assertThat(#{any()}).as(#{any(String)}).isCloseTo(#{any()}, within(#{any()}));") :
                     JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isCloseTo(#{any()}, within(#{any()}));");
             return template
-                    .contextSensitive()
                     .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                     .imports("java.util.function.Supplier")
-                    .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3.24"))
+                    .javaParser(assertionsParser(ctx))
                     .build()
                     .apply(
                             getCursor(),

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertFalseToAssertThat.java
@@ -97,7 +97,10 @@ public class JUnitAssertFalseToAssertThat extends Recipe {
                         );
             }
 
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
+            //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false););
+
+            // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
 
             return method;

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNotEqualsToAssertThat.java
@@ -74,7 +74,6 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
 
             if (args.size() == 2) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotEqualTo(#{any()});")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -93,7 +92,6 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
 
 
                 method = template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -106,7 +104,6 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                         );
             } else if (args.size() == 3) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotCloseTo(#{any()}, within(#{any()}));")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -117,7 +114,7 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                                 expected,
                                 args.get(2)
                         );
-                maybeAddImport("org.assertj.core.api.Assertions", "within");
+                maybeAddImport("org.assertj.core.api.Assertions", "within", false);
             } else {
                 Expression message = args.get(3);
 
@@ -126,7 +123,6 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNotCloseTo(#{any()}, within(#{any()}));");
 
                 method = template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat", "org.assertj.core.api.Assertions.within")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -139,12 +135,13 @@ public class JUnitAssertNotEqualsToAssertThat extends Recipe {
                                 args.get(2)
                         );
 
-                maybeAddImport("org.assertj.core.api.Assertions", "within");
+                maybeAddImport("org.assertj.core.api.Assertions", "within", false);
             }
 
-            //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat"
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
-            //And if there are no longer references to the JUnit assertions class, we can remove the import.
+            //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
+            // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
 
             return method;

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNotNullToAssertThat.java
@@ -71,7 +71,6 @@ public class JUnitAssertNotNullToAssertThat extends Recipe {
 
             if (args.size() == 1) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNotNull();")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -89,7 +88,6 @@ public class JUnitAssertNotNullToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNotNull();");
 
                 method = template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -101,8 +99,11 @@ public class JUnitAssertNotNullToAssertThat extends Recipe {
                         );
             }
 
+            //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
+            //And if there are no longer references to the JUnit assertions class, we can remove the import.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
 
             return method;
         }

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertNullToAssertThat.java
@@ -71,7 +71,6 @@ public class JUnitAssertNullToAssertThat extends Recipe {
 
             if (args.size() == 1) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isNull();")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -88,7 +87,6 @@ public class JUnitAssertNullToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isNull();");
 
                 method = template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -100,11 +98,11 @@ public class JUnitAssertNullToAssertThat extends Recipe {
                         );
             }
 
+            // Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
             // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
-
-            // Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat".
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
 
             return method;
         }

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertSameToAssertThat.java
@@ -72,7 +72,6 @@ public class JUnitAssertSameToAssertThat extends Recipe {
 
             if (args.size() == 2) {
                 method = JavaTemplate.builder("assertThat(#{any()}).isSameAs(#{any()});")
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -90,7 +89,6 @@ public class JUnitAssertSameToAssertThat extends Recipe {
                         JavaTemplate.builder("assertThat(#{any()}).as(#{any(java.util.function.Supplier)}).isSameAs(#{any()});");
 
                 method = template
-                        .contextSensitive()
                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                         .javaParser(assertionsParser(ctx))
                         .build()
@@ -103,8 +101,11 @@ public class JUnitAssertSameToAssertThat extends Recipe {
                         );
             }
 
-            maybeRemoveImport("org.junit.jupiter.api.Assertions");
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
+            // Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
+            // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
+            maybeRemoveImport(JUNIT_QUALIFIED_ASSERTIONS_CLASS_NAME);
 
             return method;
         }

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertThrowsToAssertExceptionType.java
@@ -85,7 +85,7 @@ public class JUnitAssertThrowsToAssertExceptionType extends Recipe {
                                     mi.getCoordinates().replace(),
                                     mi.getArguments().get(0), executable
                             );
-                    maybeAddImport("org.assertj.core.api.AssertionsForClassTypes", "assertThatExceptionOfType");
+                    maybeAddImport("org.assertj.core.api.AssertionsForClassTypes", "assertThatExceptionOfType", false);
                     maybeRemoveImport("org.junit.jupiter.api.Assertions.assertThrows");
                     maybeRemoveImport("org.junit.jupiter.api.Assertions");
                 }

src/main/java/org/openrewrite/java/testing/assertj/JUnitAssertTrueToAssertThat.java
@@ -97,7 +97,10 @@ public class JUnitAssertTrueToAssertThat extends Recipe {
                         );
             }
 
-            maybeAddImport("org.assertj.core.api.Assertions", "assertThat");
+            //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+            maybeAddImport("org.assertj.core.api.Assertions", "assertThat", false);
+
+            // Remove import for "org.junit.jupiter.api.Assertions" if no longer used.
             maybeRemoveImport("org.junit.jupiter.api.Assertions");
 
             return method;

src/main/java/org/openrewrite/java/testing/assertj/JUnitFailToAssertJFail.java
@@ -145,7 +145,8 @@ public class JUnitFailToAssertJFail extends Recipe {
                                 method.getCoordinates().replace(),
                                 arguments.toArray()
                         );
-                maybeAddImport("org.assertj.core.api.Assertions", "fail");
+                //Make sure there is a static import for "org.assertj.core.api.Assertions.assertThat" (even if not referenced)
+                maybeAddImport("org.assertj.core.api.Assertions", "fail", false);
                 return super.visitMethodInvocation(method, ctx);
             }
         }