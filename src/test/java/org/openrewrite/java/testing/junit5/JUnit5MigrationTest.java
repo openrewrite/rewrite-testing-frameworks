3480d1ae406da39ceca87b9dfd68ceae03386699
Tim te Beek <tim@moderne.io>
2024-05-22 22:17:24 +0200

    Add missing document example annotation

--- src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.testing.junit5;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Issue;
 import org.openrewrite.Tree;
@@ -74,6 +75,7 @@ class JUnit5MigrationTest implements RewriteTest {
         );
     }
 
+    @DocumentExample
     @Test
     @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/145")
     void assertThatReceiver() {
