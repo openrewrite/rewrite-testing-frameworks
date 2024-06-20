8388b2898794e4ff0bb26d404d4c173246ff14b1
Shivani Sharma <s.happyrose@gmail.com>
2024-06-20 20:59:50 +1000

    Rewrite both JMockit  `@Mocked` and `@Injectable` annotated arguments (#532)
    * Ensure Jmockit expectations with no times or result transform to a mockito verify
    
    * Minor polish to text blocks
    
    * Make times, minTimes, maxTimes more flexible as it was originally so even if someone mistakenly puts times and minTimes together, it still migrates without issue
    
    * Add feature to enable migration of Jmockit Injectable annotation exactly as we are doing the Mocked annotation ie method parameter annotation as well as field annotation. Have moved all of the code from JMockitMockedVariableToMockito to JMockitAnnotationToMockito for code reuse. Also add the corresponding test cases and jmockit.yml file modification.
    
    * Use `@Option` instead of inheritance
    
    * Drop Option and just replace both variants
    
    * Update display name and description to match application
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/java/testing/jmockit/JMockitAnnotatedArgumentToMockitoTest.java
+++ src/test/java/org/openrewrite/java/testing/jmockit/JMockitAnnotatedArgumentToMockitoTest.java
@@ -24,7 +24,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-class JMockitMockedVariableToMockitoTest implements RewriteTest {
+class JMockitAnnotatedArgumentToMockitoTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec
@@ -32,9 +32,7 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
             .logCompilationWarningsAndErrors(true)
             .classpathFromResources(new InMemoryExecutionContext(),
               "junit-jupiter-api-5.9",
-              "jmockit-1.49",
-              "mockito-core-3.12",
-              "mockito-junit-jupiter-3.12"
+              "jmockit-1.49"
             ))
           .recipeFromResource(
             "/META-INF/rewrite/jmockit.yml",
@@ -52,7 +50,7 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
               import mockit.Mocked;
               
               import static org.junit.jupiter.api.Assertions.assertNotNull;
-                          
+              
               class A {
                   @Mocked
                   Object mockedObject;
@@ -68,7 +66,7 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
               import org.mockito.Mockito;
               
               import static org.junit.jupiter.api.Assertions.assertNotNull;
-                          
+              
               class A {
                   @Mock
                   Object mockedObject;
@@ -86,7 +84,7 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
     }
 
     @Test
-    void noVariableTest() {
+    void mockedNoVariableTest() {
         rewriteRun(
           //language=java
           java(
@@ -94,7 +92,7 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
               import mockit.Mocked;
               
               import static org.junit.jupiter.api.Assertions.assertNotNull;
-                          
+              
               class A {
                   @Mocked
                   Object mockedObject;
@@ -108,7 +106,86 @@ class JMockitMockedVariableToMockitoTest implements RewriteTest {
               import org.mockito.Mock;
               
               import static org.junit.jupiter.api.Assertions.assertNotNull;
-                          
+              
+              class A {
+                  @Mock
+                  Object mockedObject;
+              
+                  void test() {
+                      assertNotNull(mockedObject);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void injectableVariableTest() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              import mockit.Injectable;
+              
+              import static org.junit.jupiter.api.Assertions.assertNotNull;
+              
+              class A {
+                  @Injectable
+                  Object mockedObject;
+              
+                  void test(@Injectable Object o, @Injectable Object o2) {
+                      assertNotNull(o);
+                      assertNotNull(o2);
+                  }
+              }
+              """,
+            """
+              import org.mockito.Mock;
+              import org.mockito.Mockito;
+              
+              import static org.junit.jupiter.api.Assertions.assertNotNull;
+              
+              class A {
+                  @Mock
+                  Object mockedObject;
+              
+                  void test() {
+                      Object o = Mockito.mock(Object.class);
+                      Object o2 = Mockito.mock(Object.class);
+                      assertNotNull(o);
+                      assertNotNull(o2);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void injectableNoVariableTest() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import mockit.Injectable;
+              
+              import static org.junit.jupiter.api.Assertions.assertNotNull;
+              
+              class A {
+                  @Injectable
+                  Object mockedObject;
+              
+                  void test() {
+                      assertNotNull(mockedObject);
+                  }
+              }
+              """,
+            """
+              import org.mockito.Mock;
+              
+              import static org.junit.jupiter.api.Assertions.assertNotNull;
+              
               class A {
                   @Mock
                   Object mockedObject;
