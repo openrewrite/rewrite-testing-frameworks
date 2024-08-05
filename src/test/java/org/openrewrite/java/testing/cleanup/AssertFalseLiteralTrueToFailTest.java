7e50c1e7da7382910aa423934ea9d3de4e34befe
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-08-05 16:28:04 +0200

    Refactor to refaster recipe

--- src/test/java/org/openrewrite/java/testing/cleanup/AssertFalseLiteralTrueToFailTest.java
+++ src/test/java/org/openrewrite/java/testing/cleanup/AssertFalseLiteralTrueToFailTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.cleanup;
 
 import org.junit.jupiter.api.Test;
@@ -14,7 +29,7 @@ class AssertFalseLiteralTrueToFailTest implements RewriteTest {
     public void defaults(RecipeSpec spec) {
         spec
           .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "junit-jupiter-api-5.9"))
-          .recipe(new AssertFalseLiteralTrueToFail());
+          .recipe(new AssertLiteralBooleanToFailRecipe());
     }
 
     @Test
@@ -50,7 +65,7 @@ class AssertFalseLiteralTrueToFailTest implements RewriteTest {
         rewriteRun(
           java(
             """
-              import org.junit.jupiter.api.Assertions;
+              import org.junit.jupiter.api.*;
                             
               public class Test {
                   void test() {
@@ -59,7 +74,7 @@ class AssertFalseLiteralTrueToFailTest implements RewriteTest {
               }
               """,
             """
-              import org.junit.jupiter.api.Assertions;
+              import org.junit.jupiter.api.*;
                             
               public class Test {
                   void test() {
@@ -77,7 +92,7 @@ class AssertFalseLiteralTrueToFailTest implements RewriteTest {
         rewriteRun(
           java(
             """
-              import static org.junit.jupiter.api.Assertions.assertFalse;
+              import static org.junit.jupiter.api.Assertions.*;
                             
               public class Test {
                   void test() {
