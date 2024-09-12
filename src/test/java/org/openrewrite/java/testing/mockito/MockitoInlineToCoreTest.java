761d0e93061ca1aa7ccb2ff32f7fb16d6310e3fd
Tim te Beek <tim@moderne.io>
2024-09-12 14:41:42 +0200

    Inline variables and adopt `getFirst()`

--- src/test/java/org/openrewrite/java/testing/mockito/MockitoInlineToCoreTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/MockitoInlineToCoreTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.testing.mockito;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -29,6 +30,7 @@ class MockitoInlineToCoreTest implements RewriteTest {
         spec.recipeFromResources("org.openrewrite.java.testing.mockito.Mockito1to5Migration");
     }
 
+    @DocumentExample
     @Test
     void inlineToCore() {
         rewriteRun(
@@ -50,25 +52,22 @@ class MockitoInlineToCoreTest implements RewriteTest {
                 </dependencies>
               </project>
               """,
-            sourceSpecs -> sourceSpecs.after(after -> {
-                String version = Pattern.compile("<version>(5.+)</version>").matcher(after).results().reduce((a, b) -> b).orElseThrow().group(1);
-                return """
-                  <project>
-                    <modelVersion>4.0.0</modelVersion>
-                    <groupId>com.example</groupId>
-                    <artifactId>demo</artifactId>
-                    <version>0.0.1-SNAPSHOT</version>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.mockito</groupId>
-                            <artifactId>mockito-core</artifactId>
-                            <version>%s</version>
-                            <scope>test</scope>
-                        </dependency>
-                    </dependencies>
-                  </project>
-                  """.formatted(version);
-            })
+            spec -> spec.after(after -> """
+              <project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.example</groupId>
+                <artifactId>demo</artifactId>
+                <version>0.0.1-SNAPSHOT</version>
+                <dependencies>
+                    <dependency>
+                        <groupId>org.mockito</groupId>
+                        <artifactId>mockito-core</artifactId>
+                        <version>%s</version>
+                        <scope>test</scope>
+                    </dependency>
+                </dependencies>
+              </project>
+              """.formatted(Pattern.compile("<version>(5.+)</version>").matcher(after).results().findFirst().orElseThrow().group(1)))
           )
         );
     }
@@ -100,25 +99,22 @@ class MockitoInlineToCoreTest implements RewriteTest {
                 </dependencies>
               </project>
               """,
-            sourceSpecs -> sourceSpecs.after(after -> {
-                String version = Pattern.compile("<version>(5.+)</version>").matcher(after).results().reduce((a, b) -> b).orElseThrow().group(1);
-                return """
-                  <project>
-                    <modelVersion>4.0.0</modelVersion>
-                    <groupId>com.example</groupId>
-                    <artifactId>demo</artifactId>
-                    <version>0.0.1-SNAPSHOT</version>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.mockito</groupId>
-                            <artifactId>mockito-core</artifactId>
-                            <version>%s</version>
-                            <scope>test</scope>
-                        </dependency>
-                    </dependencies>
-                  </project>
-                  """.formatted(version);
-            })
+            spec -> spec.after(after -> """
+              <project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.example</groupId>
+                <artifactId>demo</artifactId>
+                <version>0.0.1-SNAPSHOT</version>
+                <dependencies>
+                    <dependency>
+                        <groupId>org.mockito</groupId>
+                        <artifactId>mockito-core</artifactId>
+                        <version>%s</version>
+                        <scope>test</scope>
+                    </dependency>
+                </dependencies>
+              </project>
+              """.formatted(Pattern.compile("<version>(5.+)</version>").matcher(after).results().findFirst().orElseThrow().group(1)))
           )
         );
     }
@@ -150,32 +146,30 @@ class MockitoInlineToCoreTest implements RewriteTest {
                 </dependencies>
               </project>
               """,
-            sourceSpecs -> sourceSpecs.after(after -> {
-                String mockitoVersion = Pattern.compile("<version>(5.+)</version>").matcher(after).results().reduce((a, b) -> b).orElseThrow().group(1);
-                String byteBuddyVersion = Pattern.compile("<version>(1.15.+)</version>").matcher(after).results().reduce((a, b) -> b).orElseThrow().group(1);
-                return """
-                  <project>
-                    <modelVersion>4.0.0</modelVersion>
-                    <groupId>com.example</groupId>
-                    <artifactId>demo</artifactId>
-                    <version>0.0.1-SNAPSHOT</version>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.mockito</groupId>
-                            <artifactId>mockito-core</artifactId>
-                            <version>%s</version>
-                            <scope>test</scope>
-                        </dependency>
-                        <dependency>
-                            <groupId>net.bytebuddy</groupId>
-                            <artifactId>byte-buddy</artifactId>
-                            <version>%s</version>
-                            <scope>test</scope>
-                        </dependency>
-                    </dependencies>
-                  </project>
-                  """.formatted(mockitoVersion, byteBuddyVersion);
-            })
+            spec -> spec.after(after -> """
+              <project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.example</groupId>
+                <artifactId>demo</artifactId>
+                <version>0.0.1-SNAPSHOT</version>
+                <dependencies>
+                    <dependency>
+                        <groupId>org.mockito</groupId>
+                        <artifactId>mockito-core</artifactId>
+                        <version>%s</version>
+                        <scope>test</scope>
+                    </dependency>
+                    <dependency>
+                        <groupId>net.bytebuddy</groupId>
+                        <artifactId>byte-buddy</artifactId>
+                        <version>%s</version>
+                        <scope>test</scope>
+                    </dependency>
+                </dependencies>
+              </project>
+              """.formatted(
+              Pattern.compile("<version>(5.+)</version>").matcher(after).results().findFirst().orElseThrow().group(1),
+              Pattern.compile("<version>(1.15.+)</version>").matcher(after).results().findFirst().orElseThrow().group(1)))
           )
         );
     }
