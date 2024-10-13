dd8aacb3ebf8994e8fdd5996bb783ea9bad40f02
Tim te Beek <tim@moderne.io>
2024-10-13 14:35:58 +0200

    Remove unused import

--- src/test/java/org/openrewrite/java/testing/mockito/SimplifyMockitoVerifyWhenGivenTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/SimplifyMockitoVerifyWhenGivenTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
-import org.openrewrite.java.testing.mockito.SimplifyMockitoVerifyWhenGiven;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
