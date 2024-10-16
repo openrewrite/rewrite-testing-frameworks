ff29dbc48e04c5e2b458c88f80107667874a834e
Tim te Beek <tim@moderne.io>
2024-10-16 13:48:30 +0200

    Disable acyclic cursor validation for PowerMock tests

--- src/test/java/org/openrewrite/java/testing/mockito/PowerMockitoMockStaticToMockitoTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/PowerMockitoMockStaticToMockitoTest.java
@@ -22,6 +22,7 @@ import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.TypeValidation;
 
 import static org.openrewrite.groovy.Assertions.groovy;
 import static org.openrewrite.java.Assertions.java;
@@ -41,7 +42,8 @@ class PowerMockitoMockStaticToMockitoTest implements RewriteTest {
               "powermock-core-1.6",
               "testng-7.7"
             ))
-          .recipe(new PowerMockitoMockStaticToMockito());
+          .recipe(new PowerMockitoMockStaticToMockito())
+          .typeValidationOptions(TypeValidation.builder().cursorAcyclic(false).build());
     }
 
     @DocumentExample
