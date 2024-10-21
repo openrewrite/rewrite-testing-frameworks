49b3eb54bebb5a349263727b76b9d5584d971be4
Tim te Beek <tim@moderne.io>
2024-10-21 22:38:12 +0200

    Expand imports on EnclosedToNested

--- src/main/java/org/openrewrite/java/testing/junit5/EnclosedToNested.java
+++ src/main/java/org/openrewrite/java/testing/junit5/EnclosedToNested.java
@@ -21,7 +21,9 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.java.*;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.RemoveAnnotationVisitor;
 import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
 
