559fb5154ccf564dd4fb4dc5e4a3b6dfbfc2aaf7
lingenj <jacob.van.lingen@moderne.io>
2024-11-11 09:41:23 +0100

    Improve `EasyMockVerifyToMockitoVerify` recipe

--- src/main/java/org/openrewrite/java/testing/easymock/EasyMockVerifyToMockitoVerify.java
+++ src/main/java/org/openrewrite/java/testing/easymock/EasyMockVerifyToMockitoVerify.java
@@ -29,11 +29,11 @@ import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaCoordinates;
 import org.openrewrite.java.tree.Statement;
 
-import static java.lang.String.join;
-import static java.util.Collections.nCopies;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.String.join;
+import static java.util.Collections.nCopies;
 
 public class EasyMockVerifyToMockitoVerify extends Recipe {
 
@@ -55,48 +55,49 @@ public class EasyMockVerifyToMockitoVerify extends Recipe {
         return Preconditions.check(new UsesMethod<>(VERIFY_MATCHER), new JavaIsoVisitor<ExecutionContext>() {
             @Override
             public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-                J.MethodDeclaration md = method;
+                J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
+                if (md.getBody() == null) {
+                    return md;
+                }
 
                 maybeAddImport("org.mockito.Mockito", "verify");
                 maybeRemoveImport("org.easymock.EasyMock.verify");
 
-                if (md.getBody() != null) {
-                    int idx = 0;
-                    for (Statement statement : md.getBody().getStatements()) {
-                        if (statement instanceof J.MethodInvocation) {
-                            J.MethodInvocation m = (J.MethodInvocation) statement;
-                            if (VERIFY_MATCHER.matches(m) && m.getArguments().size() == 1 && m.getArguments().get(0) instanceof J.Identifier) {
-                                J.Identifier dependency =  (J.Identifier) m.getArguments().get(0);
-                                List<Statement> statementsAboveVerify = md.getBody().getStatements().subList(0, idx);
-                                List<J.MethodInvocation> expectedCalls = getExpectedCalls(dependency, statementsAboveVerify);
+                int idx = 0;
+                for (Statement statement : md.getBody().getStatements()) {
+                    if (statement instanceof J.MethodInvocation) {
+                        J.MethodInvocation m = (J.MethodInvocation) statement;
+                        if (VERIFY_MATCHER.matches(m) && m.getArguments().size() == 1 && m.getArguments().get(0) instanceof J.Identifier) {
+                            J.Identifier dependency = (J.Identifier) m.getArguments().get(0);
+                            List<Statement> statementsAboveVerify = md.getBody().getStatements().subList(0, idx);
+                            List<J.MethodInvocation> expectedCalls = getExpectedCalls(dependency, statementsAboveVerify);
 
-                                for (int i = 0, expectedCallsSize = expectedCalls.size(); i < expectedCallsSize; i++) {
-                                    J.MethodInvocation expectedMethod = expectedCalls.get(i);
-                                    List<Expression> parameters = expectedMethod.getArguments();
-                                    if (parameters.size() == 1 && parameters.get(0) instanceof J.Empty) {
-                                        parameters.clear();
-                                    }
-                                    String anyArgs = join(",", nCopies(parameters.size(), "#{any()}"));
-                                    parameters.add(0, dependency);
-                                    Statement currStatement = md.getBody().getStatements().get(idx);
-                                    JavaCoordinates coordinates = i == 0 ? currStatement.getCoordinates().replace() : currStatement.getCoordinates().after();
-                                    md = JavaTemplate.builder("verify(#{any()})." + expectedMethod.getSimpleName() + "(" + anyArgs + ")")
-                                            .contextSensitive()
-                                            .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "mockito-core-5"))
-                                            .staticImports("org.mockito.Mockito.verify")
-                                            .build()
-                                            .apply(updateCursor(md), coordinates, parameters.toArray());
-                                    if (i != 0) {
-                                        idx++;
-                                    }
+                            for (int i = 0, expectedCallsSize = expectedCalls.size(); i < expectedCallsSize; i++) {
+                                J.MethodInvocation expectedMethod = expectedCalls.get(i);
+                                List<Expression> parameters = expectedMethod.getArguments();
+                                if (parameters.size() == 1 && parameters.get(0) instanceof J.Empty) {
+                                    parameters.clear();
+                                }
+                                String anyArgs = join(",", nCopies(parameters.size(), "#{any()}"));
+                                parameters.add(0, dependency);
+                                Statement currStatement = md.getBody().getStatements().get(idx);
+                                JavaCoordinates coordinates = i == 0 ? currStatement.getCoordinates().replace() : currStatement.getCoordinates().after();
+                                md = JavaTemplate.builder("verify(#{any()})." + expectedMethod.getSimpleName() + "(" + anyArgs + ")")
+                                        .contextSensitive()
+                                        .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "mockito-core-5"))
+                                        .staticImports("org.mockito.Mockito.verify")
+                                        .build()
+                                        .apply(updateCursor(md), coordinates, parameters.toArray());
+                                if (i != 0) {
+                                    idx++;
                                 }
                             }
                         }
-                        idx++;
                     }
+                    idx++;
                 }
 
-                return super.visitMethodDeclaration(md, ctx);
+                return md;
             }
 
             private List<J.MethodInvocation> getExpectedCalls(J.Identifier dependency, List<Statement> statementsAboveVerify) {
