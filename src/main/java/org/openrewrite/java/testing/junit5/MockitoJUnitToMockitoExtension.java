89968a524d76b592832151016ee975ebdc1657d2
Tim te Beek <timtebeek@gmail.com>
2024-11-14 09:31:56 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/java/testing/junit5/MockitoJUnitToMockitoExtension.java
+++ src/main/java/org/openrewrite/java/testing/junit5/MockitoJUnitToMockitoExtension.java
@@ -19,7 +19,6 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
