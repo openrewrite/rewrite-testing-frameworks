4e91f081cf332359b3e573800111ddab8c43758e
Tim te Beek <tim@moderne.io>
2024-11-17 14:52:50 +0100

    Switch to `visitMethodInvocation` & change method type too

--- src/main/java/org/openrewrite/java/testing/junit5/UseAssertSame.java
+++ src/main/java/org/openrewrite/java/testing/junit5/UseAssertSame.java
@@ -19,12 +19,12 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,13 +32,13 @@ import java.util.List;
 public class UseAssertSame extends Recipe {
     @Override
     public String getDisplayName() {
-        return "Use JUnit5's `assertSame` or `assertNotSame` instead of assertTrue(... == ...)";
+        return "Use JUnit5's `assertSame` or `assertNotSame` instead of `assertTrue(... == ...)`";
     }
 
     @Override
     public String getDescription() {
         return "Prefers the usage of `assertSame` or `assertNotSame` methods instead of using of vanilla assertTrue " +
-                "or assertFalse with a boolean comparison.";
+               "or assertFalse with a boolean comparison.";
     }
 
     @Override
@@ -48,45 +48,37 @@ public class UseAssertSame extends Recipe {
         TreeVisitor<?, ExecutionContext> orMatcher = Preconditions.or(new UsesMethod<>(assertTrueMatcher), new UsesMethod<>(assertFalseMatcher));
         return Preconditions.check(orMatcher, new JavaIsoVisitor<ExecutionContext>() {
             @Override
-            public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration methodDecl, ExecutionContext ctx) {
-                J.MethodDeclaration m = super.visitMethodDeclaration(methodDecl, ctx);
-                if (m.getBody() == null) {
-                    return m;
+            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation methodInvocation, ExecutionContext ctx) {
+                J.MethodInvocation mi = super.visitMethodInvocation(methodInvocation, ctx);
+                if (!assertTrueMatcher.matches(mi) && !assertFalseMatcher.matches(mi)) {
+                    return mi;
                 }
-                return m.withBody(m.getBody().withStatements(ListUtils.flatMap(m.getBody().getStatements(), methodStatement -> {
-                    if (!(methodStatement instanceof J.MethodInvocation)) {
-                        return methodStatement;
-                    }
-                    J.MethodInvocation methodInvocation = (J.MethodInvocation) methodStatement;
-                    if (!assertTrueMatcher.matches(methodInvocation) && !assertFalseMatcher.matches(methodInvocation)) {
-                        return methodStatement;
-                    }
 
-                    Expression firstArgument = methodInvocation.getArguments().get(0);
-                    List<Expression> argumentsTail = methodInvocation.getArguments().subList(1, methodInvocation.getArguments().size());
-                    if (!(firstArgument instanceof J.Binary)) {
-                        return methodStatement;
-                    }
-                    J.Binary binary = (J.Binary) firstArgument;
-                    if (binary.getOperator() != J.Binary.Type.Equal && binary.getOperator() != J.Binary.Type.NotEqual) {
-                        return methodStatement;
-                    }
-                    boolean positive = binary.getOperator() == J.Binary.Type.Equal == assertTrueMatcher.matches(methodInvocation);
-                    List<Expression> newArguments = new ArrayList<>();
-                    newArguments.add(binary.getLeft());
-                    newArguments.add(binary.getRight());
-                    newArguments.addAll(argumentsTail);
+                Expression firstArgument = mi.getArguments().get(0);
+                if (!(firstArgument instanceof J.Binary)) {
+                    return mi;
+                }
+                J.Binary binary = (J.Binary) firstArgument;
+                if (binary.getOperator() != J.Binary.Type.Equal && binary.getOperator() != J.Binary.Type.NotEqual) {
+                    return mi;
+                }
+                boolean positive = binary.getOperator() == J.Binary.Type.Equal == assertTrueMatcher.matches(mi);
+                List<Expression> newArguments = new ArrayList<>();
+                newArguments.add(binary.getLeft());
+                newArguments.add(binary.getRight());
+                newArguments.addAll(mi.getArguments().subList(1, mi.getArguments().size()));
 
-                    String newMethodName = positive ? "assertSame" : "assertNotSame";
+                String newMethodName = positive ? "assertSame" : "assertNotSame";
 
-                    maybeRemoveImport("org.junit.jupiter.api.Assertions");
-                    maybeAddImport("org.junit.jupiter.api.Assertions", newMethodName);
+                maybeRemoveImport("org.junit.jupiter.api.Assertions");
+                maybeAddImport("org.junit.jupiter.api.Assertions", newMethodName);
 
-                    return methodInvocation
-                            .withName(methodInvocation.getName().withSimpleName(newMethodName))
-                            .withArguments(newArguments);
-                })));
+                JavaType.Method newType = ((JavaType.Method) mi.getName().getType()).withName(newMethodName);
+                return mi.withName(mi.getName().withSimpleName(newMethodName).withType(newType))
+                        .withMethodType(newType)
+                        .withArguments(newArguments);
             }
         });
     }
+
 }
