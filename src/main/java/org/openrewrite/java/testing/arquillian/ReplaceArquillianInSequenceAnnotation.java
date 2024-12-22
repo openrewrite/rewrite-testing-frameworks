ff420651163f9724f80c6f2e6152501e6a1454e4
Tim te Beek <tim@moderne.io>
2024-12-22 13:02:43 +0100

    Use JavaTemplate to add the new annotation

--- src/main/java/org/openrewrite/java/testing/arquillian/ReplaceArquillianInSequenceAnnotation.java
+++ src/main/java/org/openrewrite/java/testing/arquillian/ReplaceArquillianInSequenceAnnotation.java
@@ -19,20 +19,12 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.java.ChangeType;
-import org.openrewrite.java.JavaIsoVisitor;
-import org.openrewrite.java.search.FindAnnotations;
+import org.openrewrite.java.*;
 import org.openrewrite.java.search.UsesType;
-import org.openrewrite.java.tree.*;
-import org.openrewrite.marker.Markers;
+import org.openrewrite.java.service.AnnotationService;
+import org.openrewrite.java.tree.J;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-import static java.util.Collections.emptyList;
-import static org.openrewrite.Tree.randomId;
+import java.util.Comparator;
 
 public class ReplaceArquillianInSequenceAnnotation extends Recipe {
 
@@ -48,74 +40,50 @@ public class ReplaceArquillianInSequenceAnnotation extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return Preconditions.check(new UsesType<>("org.jboss.arquillian.junit.InSequence", false),
-                new InSequenceToOrderVisitor());
-    }
+        return Preconditions.check(
+                new UsesType<>("org.jboss.arquillian.junit.InSequence", false),
+                new JavaIsoVisitor<ExecutionContext>() {
+                    private final String IN_SEQUENCE = "org.jboss.arquillian.junit.InSequence";
+                    private final String TEST_METHOD_ORDER = "org.junit.jupiter.api.TestMethodOrder";
+                    private final String METHOD_ORDERER = "org.junit.jupiter.api.MethodOrderer";
+                    private final AnnotationMatcher IN_SEQUENCE_MATCHER = new AnnotationMatcher("@" + IN_SEQUENCE);
+                    private final AnnotationMatcher TEST_METHOD_ORDER_MATCHER = new AnnotationMatcher("@" + TEST_METHOD_ORDER);
 
-    public static class InSequenceToOrderVisitor extends JavaIsoVisitor<ExecutionContext> {
-        private static final JavaType.Class testMethodOrderType = JavaType.ShallowClass.build("org.junit.jupiter.api.TestMethodOrder");
-        private static final JavaType.Class methodOrderType = JavaType.ShallowClass.build("org.junit.jupiter.api.MethodOrderer");
-        private static final JavaType.Class orderType = JavaType.ShallowClass.build("org.junit.jupiter.api.Order");
-
-        @Override
-        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
-
-
-
-            return cd;
-        }
-
-        @Override
-        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-            J.MethodDeclaration m = super.visitMethodDeclaration(method, ctx);
-            Set<J.Annotation> inSequenceAnnotations = FindAnnotations.find(m, "@org.jboss.arquillian.junit.InSequence");
-            if (!inSequenceAnnotations.isEmpty()) {
-                doAfterVisit(new ChangeType(
-                        "org.jboss.arquillian.junit.InSequence",
-                        "org.junit.jupiter.api.Order",
-                        true).getVisitor());
-
-                J.ClassDeclaration classDeclaration = getCursor().dropParentUntil(org.openrewrite.java.tree.J.ClassDeclaration.class::isInstance)
-                        .getValue();
-                doAfterVisit(new JavaIsoVisitor<ExecutionContext>() {
                     @Override
                     public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
                         J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
-                        if (cd.getName().equals(classDeclaration.getName())) {
-                            if (FindAnnotations.find(cd, "@" + testMethodOrderType.getFullyQualifiedName()).isEmpty()) {
-                                List<J.Annotation> annotations = new ArrayList<>(cd.getLeadingAnnotations());
-                                annotations.add(new J.Annotation(randomId(), Space.EMPTY, Markers.EMPTY,
-                                        new J.Identifier(randomId(), Space.EMPTY, Markers.EMPTY, emptyList(), testMethodOrderType.getClassName(), testMethodOrderType, null),
-                                        JContainer.build(Space.EMPTY,
-                                                Collections.singletonList(
-                                                        new JRightPadded<>(
-                                                                new J.Literal(
-                                                                        randomId(),
-                                                                        Space.EMPTY,
-                                                                        Markers.EMPTY,
-                                                                        methodOrderType,
-                                                                        methodOrderType.getClassName() + ".class",
-                                                                        null,
-                                                                        JavaType.Primitive.None),
-                                                                Space.EMPTY,
-                                                                Markers.EMPTY
-                                                        )
-                                                ),
-                                                Markers.EMPTY
-                                        )
-                                ));
-                                cd = cd.withLeadingAnnotations(annotations);
-                                maybeAddImport(testMethodOrderType);
-                                maybeAddImport(methodOrderType.getFullyQualifiedName(), null, false);
-                            }
-                        }
-                        cd = maybeAutoFormat(classDecl, cd, cd.getName(), ctx, getCursor().getParentTreeCursor());
+                        doAfterVisit(new ChangeType(IN_SEQUENCE, "org.junit.jupiter.api.Order", true).getVisitor());
                         return cd;
                     }
-                });
-            }
-            return maybeAutoFormat(method, m, m.getName(), ctx, getCursor().getParentTreeCursor());
-        }
+
+                    @Override
+                    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+                        J.MethodDeclaration m = super.visitMethodDeclaration(method, ctx);
+                        if (service(AnnotationService.class).matches(updateCursor(m), IN_SEQUENCE_MATCHER)) {
+                            J.ClassDeclaration classWithInSequenceMethods = getCursor().firstEnclosing(J.ClassDeclaration.class);
+                            if (classWithInSequenceMethods != null) {
+                                doAfterVisit(new JavaIsoVisitor<ExecutionContext>() {
+                                    @Override
+                                    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                                        J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+                                        if (cd.getName().equals(classWithInSequenceMethods.getName()) &&
+                                                !service(AnnotationService.class).matches(updateCursor(cd), TEST_METHOD_ORDER_MATCHER)) {
+                                            maybeAddImport(METHOD_ORDERER);
+                                            maybeAddImport(TEST_METHOD_ORDER);
+                                            return JavaTemplate.builder("@TestMethodOrder(MethodOrderer.class)")
+                                                    .imports(METHOD_ORDERER, TEST_METHOD_ORDER)
+                                                    .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "junit-jupiter-api-5"))
+                                                    .build()
+                                                    .apply(getCursor(), classDecl.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName)));
+                                        }
+                                        return cd;
+                                    }
+                                });
+                            }
+                        }
+                        return m;
+                    }
+                }
+        );
     }
 }
