261cf00c178bd477ff6fb047d91c2e236b406313
Tim te Beek <tim@moderne.io>
2024-12-23 15:03:32 +0100

    Light polish to reduce duplication

--- src/test/java/org/openrewrite/java/testing/mockito/Mockito1to5MigrationTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/Mockito1to5MigrationTest.java
@@ -22,14 +22,10 @@ import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.gradle.Assertions.buildGradle;
 import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
 import static org.openrewrite.java.Assertions.java;
-
 
 class Mockito1to5MigrationTest implements RewriteTest {
 
@@ -46,47 +42,30 @@ class Mockito1to5MigrationTest implements RewriteTest {
     @Test
     void modifyMockitoDependencies() {
         rewriteRun(
-          //language=groovy
           buildGradle(
+            //language=groovy
             """
-               plugins {
-                   id 'java-library'
-               }
-               repositories {
-                   mavenCentral()
-               }
-               dependencies {
-                   implementation("org.apache.commons:commons-lang3:3.17.0")
-                   testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-                   testImplementation("org.mockito:mockito-core:3.12.4")
-                   testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
-               }
-               test {
-                  useJUnitPlatform()
-               }
-               """,
-            spec -> spec.after(buildGradleAfter -> {
-                Matcher artifactMatcher = Pattern.compile("org\\.mockito:[\\D]+:(?<version>5\\.\\d+\\.\\d+)").matcher(buildGradleAfter);
-                assertThat(artifactMatcher.find()).describedAs("Expected 5.x.y in %s", buildGradleAfter).isTrue();
-                String versionNumber = artifactMatcher.group("version");
-                return """
-               plugins {
-                   id 'java-library'
-               }
-               repositories {
-                   mavenCentral()
-               }
-               dependencies {
-                   implementation("org.apache.commons:commons-lang3:3.17.0")
-                   testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-                   testImplementation("org.mockito:mockito-core:%s")
-                   testImplementation("org.mockito:mockito-junit-jupiter:%s")
-               }
-               test {
-                  useJUnitPlatform()
-               }
-                  """.formatted(versionNumber, versionNumber);
-          })),
+              plugins {
+                  id 'java-library'
+              }
+              repositories {
+                  mavenCentral()
+              }
+              dependencies {
+                  implementation("org.apache.commons:commons-lang3:3.17.0")
+                  testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+                  testImplementation("org.mockito:mockito-core:3.12.4")
+                  testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
+              }
+              test {
+                 useJUnitPlatform()
+              }
+              """,
+            spec -> spec.after(buildGradleAfter -> assertThat(buildGradleAfter)
+              .containsPattern("org.apache.commons:commons-lang3:3.17.0")
+              .containsPattern("org.junit.jupiter:junit-jupiter-api:5.11.4")
+              .containsPattern("org\\.mockito:[\\D]+:(?<version>5\\.\\d+\\.\\d+)")
+              .actual())),
           //language=java
           java(
             """
@@ -97,9 +76,9 @@ class Mockito1to5MigrationTest implements RewriteTest {
               import java.util.List;
 
               @ExtendWith(MockitoExtension.class)
-              public class MyTest {
+              class MyTest {
                   @Test
-                  public void test() {
+                  void test() {
                       List<String> list = Mockito.mock(List.class);
                   }
               }
