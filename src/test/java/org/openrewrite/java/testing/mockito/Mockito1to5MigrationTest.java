90f6d63db55c2e197016371b249db4f100846bbb
sullis <github@seansullivan.com>
2024-12-23 06:04:42 -0800

    Add `Mockito1to5MigrationTest` to verify dependencies are upgraded (#656)
    * add Mockito1to5MigrationTest
    
    * Update src/test/java/org/openrewrite/java/testing/mockito/Mockito1to5MigrationTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/test/java/org/openrewrite/java/testing/mockito/Mockito1to5MigrationTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Light polish to reduce duplication
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/java/testing/mockito/Mockito1to5MigrationTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
+import static org.openrewrite.java.Assertions.java;
+
+class Mockito1to5MigrationTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .beforeRecipe(withToolingApi())
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "junit-jupiter-api", "mockito-core", "mockito-junit-jupiter"))
+          .recipeFromResources("org.openrewrite.java.testing.mockito.Mockito1to5Migration");
+    }
+
+    @DocumentExample
+    @Test
+    void modifyMockitoDependencies() {
+        rewriteRun(
+          buildGradle(
+            //language=groovy
+            """
+              plugins {
+                  id 'java-library'
+              }
+              repositories {
+                  mavenCentral()
+              }
+              dependencies {
+                  implementation("org.apache.commons:commons-lang3:3.17.0")
+                  testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+                  testImplementation("org.mockito:mockito-core:3.12.4")
+                  testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
+              }
+              test {
+                 useJUnitPlatform()
+              }
+              """,
+            spec -> spec.after(buildGradleAfter -> assertThat(buildGradleAfter)
+              .containsPattern("org.apache.commons:commons-lang3:3.17.0")
+              .containsPattern("org.junit.jupiter:junit-jupiter-api:5.11.4")
+              .containsPattern("org\\.mockito:[\\D]+:(?<version>5\\.\\d+\\.\\d+)")
+              .actual())),
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.Test;
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.Mockito;
+              import org.mockito.junit.jupiter.MockitoExtension;
+              import java.util.List;
+
+              @ExtendWith(MockitoExtension.class)
+              class MyTest {
+                  @Test
+                  void test() {
+                      List<String> list = Mockito.mock(List.class);
+                  }
+              }
+              """
+          )
+        );
+    }
+}
