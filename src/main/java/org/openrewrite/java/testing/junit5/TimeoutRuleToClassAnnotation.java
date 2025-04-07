1921f289377318ada38da0d3b64d0abdf115bca5
Tim te Beek <tim@moderne.io>
2025-04-07 17:44:00 +0200

    Inline the named public class, and remove the builder field

--- src/main/java/org/openrewrite/java/testing/junit5/TimeoutRuleToClassAnnotation.java
+++ src/main/java/org/openrewrite/java/testing/junit5/TimeoutRuleToClassAnnotation.java
@@ -15,7 +15,6 @@
  */
 package org.openrewrite.java.testing.junit5;
 
-import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
@@ -47,83 +46,70 @@ public class TimeoutRuleToClassAnnotation extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return Preconditions.check(new UsesType<>("org.junit.rules.Timeout", false), new TimeoutRuleToClassAnnotationVisitor());
-    }
+        return Preconditions.check(new UsesType<>("org.junit.rules.Timeout", false), new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
 
-    public static class TimeoutRuleToClassAnnotationVisitor extends JavaIsoVisitor<ExecutionContext> {
+                AtomicReference<Expression> initializer = new AtomicReference<>();
 
-        private JavaParser.@Nullable Builder<?, ?> javaParser;
-
-        private JavaParser.Builder<?, ?> javaParser(ExecutionContext ctx) {
-            if (javaParser == null) {
-                javaParser = JavaParser.fromJavaVersion()
-                        .classpathFromResources(ctx, "junit-jupiter-api-5", "hamcrest-3");
-            }
-            return javaParser;
-
-        }
-
-        @Override
-        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-            J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
-
-            AtomicReference<Expression> initializer = new AtomicReference<>();
-
-            cd = cd.withBody(cd.getBody().withStatements(ListUtils.map(cd.getBody().getStatements(), statement -> {
-                if (statement instanceof J.VariableDeclarations) {
-                    //noinspection ConstantConditions
-                    if (TypeUtils.isOfClassType(((J.VariableDeclarations) statement).getTypeExpression().getType(),
-                            "org.junit.rules.Timeout")) {
-                        List<J.VariableDeclarations.NamedVariable> variables = ((J.VariableDeclarations) statement).getVariables();
-                        if (!variables.isEmpty()) {
-                            initializer.set(variables.get(0).getInitializer());
+                cd = cd.withBody(cd.getBody().withStatements(ListUtils.map(cd.getBody().getStatements(), statement -> {
+                    if (statement instanceof J.VariableDeclarations) {
+                        //noinspection ConstantConditions
+                        if (TypeUtils.isOfClassType(((J.VariableDeclarations) statement).getTypeExpression().getType(),
+                                "org.junit.rules.Timeout")) {
+                            List<J.VariableDeclarations.NamedVariable> variables = ((J.VariableDeclarations) statement).getVariables();
+                            if (!variables.isEmpty()) {
+                                initializer.set(variables.get(0).getInitializer());
+                            }
+                            return null;
                         }
-                        return null;
                     }
-                }
-                return statement;
-            })));
+                    return statement;
+                })));
 
-            if (initializer.get() != null) {
-                cd = insertTimeoutAnnotation(initializer.get(), cd, ctx);
-                maybeRemoveImport("org.junit.Rule");
-                maybeRemoveImport("org.junit.rules.Timeout");
-            }
-            return cd;
-        }
-
-        private J.ClassDeclaration insertTimeoutAnnotation(Expression ex, J.ClassDeclaration cd, ExecutionContext ctx) {
-            JavaTemplate.Builder builder;
-            Object[] params;
-            if (ex instanceof J.NewClass) {
-                List<Expression> arguments = ((J.NewClass) ex).getArguments();
-                if (arguments.size() == 2) {
-                    builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = #{any(TimeUnit)})");
-                    params = new Object[]{arguments.get(0), arguments.get(1)};
-                } else {
-                    builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = TimeUnit.MILLISECONDS)");
-                    params = new Object[]{arguments.get(0)};
+                if (initializer.get() != null) {
+                    maybeRemoveImport("org.junit.Rule");
+                    maybeRemoveImport("org.junit.rules.Timeout");
+                    return insertTimeoutAnnotation(initializer.get(), cd, ctx);
                 }
-            } else if (ex instanceof J.MethodInvocation) {
-                String simpleName = ((J.MethodInvocation) ex).getName().getSimpleName();
-                String units = simpleName.equals("millis") ? "MILLISECONDS" : "SECONDS";
-                builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = TimeUnit." + units + ")");
-                params = new Object[]{((J.MethodInvocation) ex).getArguments().get(0)};
-            } else {
                 return cd;
             }
 
-            cd = builder.javaParser(javaParser(ctx))
-                    .imports("org.junit.jupiter.api.Timeout", "java.util.concurrent.TimeUnit")
-                    .build()
-                    .apply(
-                            updateCursor(cd),
-                            cd.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName)),
-                            params
-                    );
-            maybeAddImport("org.junit.jupiter.api.Timeout");
-            maybeAddImport("java.util.concurrent.TimeUnit");
-            return cd;
-        }
+            private J.ClassDeclaration insertTimeoutAnnotation(Expression ex, J.ClassDeclaration cd, ExecutionContext ctx) {
+                JavaTemplate.Builder builder;
+                Object[] params;
+                if (ex instanceof J.NewClass) {
+                    List<Expression> arguments = ((J.NewClass) ex).getArguments();
+                    if (arguments.size() == 2) {
+                        builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = #{any(TimeUnit)})");
+                        params = new Object[]{arguments.get(0), arguments.get(1)};
+                    } else {
+                        builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = TimeUnit.MILLISECONDS)");
+                        params = new Object[]{arguments.get(0)};
+                    }
+                } else if (ex instanceof J.MethodInvocation) {
+                    String simpleName = ((J.MethodInvocation) ex).getName().getSimpleName();
+                    String units = simpleName.equals("millis") ? "MILLISECONDS" : "SECONDS";
+                    builder = JavaTemplate.builder("@Timeout(value = #{any(long)}, unit = TimeUnit." + units + ")");
+                    params = new Object[]{((J.MethodInvocation) ex).getArguments().get(0)};
+                } else {
+                    return cd;
+                }
+
+                maybeAddImport("org.junit.jupiter.api.Timeout");
+                maybeAddImport("java.util.concurrent.TimeUnit");
+                return builder.javaParser(JavaParser.fromJavaVersion()
+                                .classpathFromResources(ctx, "junit-jupiter-api-5", "hamcrest-3"))
+                        .imports("org.junit.jupiter.api.Timeout", "java.util.concurrent.TimeUnit")
+                        .build()
+                        .apply(
+                                updateCursor(cd),
+                                cd.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName)),
+                                params
+                        );
+            }
+        });
     }
+
 }
