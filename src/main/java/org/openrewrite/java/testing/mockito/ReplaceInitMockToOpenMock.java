8e6ca2ba4cc8196da8a151c0d965c66e485b4024
Andrii Rodionov <andrii@moderne.io>
2025-04-10 16:52:32 +0200

    Recipe to replace initMocks to openMocks

---
+++ src/main/java/org/openrewrite/java/testing/mockito/ReplaceInitMockToOpenMock.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.mockito;
+
+import org.openrewrite.*;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.*;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.service.AnnotationService;
+import org.openrewrite.java.tree.J;
+
+import java.util.Objects;
+
+public class ReplaceInitMockToOpenMock extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Replace `MockitoAnnotations.initMocks(this)` to `MockitoAnnotations.openMocks(this)`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replace `MockitoAnnotations.initMocks(this)` to `MockitoAnnotations.openMocks(this)` and generate `AutoCloseable` mocks.";
+    }
+
+    private static final String MOCKITO_EXTENSION = "org.mockito.junit.jupiter.MockitoExtension";
+    private static final String MOCKITO_JUNIT_RUNNER = "org.mockito.junit.MockitoJUnitRunner";
+    private static final AnnotationMatcher BEFORE_EACH_MATCHER = new AnnotationMatcher("@org.junit.jupiter.api.BeforeEach");
+    private static final AnnotationMatcher AFTER_EACH_MATCHER = new AnnotationMatcher("@org.junit.jupiter.api.AfterEach");
+    private static final MethodMatcher INIT_MOCKS_MATCHER = new MethodMatcher("org.mockito.MockitoAnnotations initMocks(..)", false);
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                Preconditions.and(
+                        new UsesMethod<>(INIT_MOCKS_MATCHER),
+                        Preconditions.not(new UsesType<>(MOCKITO_EXTENSION, false)),
+                        Preconditions.not(new UsesType<>(MOCKITO_JUNIT_RUNNER, false))
+                ),
+                new JavaIsoVisitor<ExecutionContext>() {
+
+                    @Override
+                    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+                        J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
+                        if (service(AnnotationService.class).matches(updateCursor(md), BEFORE_EACH_MATCHER)) {
+                            md = md.withBody(md.getBody().withStatements(Objects.requireNonNull(ListUtils.map(md.getBody().getStatements(),
+                                    st -> {
+                                        if (st instanceof J.MethodInvocation && INIT_MOCKS_MATCHER.matches((J.MethodInvocation) st)) {
+                                            Cursor cursor = new Cursor(getCursor(), st);
+                                            return JavaTemplate.builder("AutoCloseable mocks = MockitoAnnotations.openMocks(this);")
+                                                    .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "mockito-core"))
+                                                    .imports("org.mockito.MockitoAnnotations")
+                                                    .build()
+                                                    .apply(cursor, st.getCoordinates().replace());
+                                        }
+                                        return st;
+                                    }))));
+
+
+                            return autoFormat(md, ctx);
+                        }
+                        return md;
+                    }
+
+
+                    @Override
+                    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration cd, ExecutionContext ctx) {
+                        cd = super.visitClassDeclaration(cd, ctx);
+
+                        return cd;
+                    }
+                }
+        );
+    }
+}
---
+++ src/test/java/org/openrewrite/java/testing/mockito/ReplaceInitMockToOpenMockTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class ReplaceInitMockToOpenMockTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "mockito-core", "mockito-junit-jupiter", "junit-4", "junit-jupiter-api-5"))
+          .recipe(new ReplaceInitMockToOpenMock());
+    }
+
+    @Test
+    @DocumentExample
+    void replaceInitMocksToOpenMocks() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.mockito.MockitoAnnotations;
+              import org.junit.jupiter.api.BeforeEach;
+
+              class A {
+
+                  @BeforeEach
+                  public void setUp() {
+                      test1();
+                      MockitoAnnotations.initMocks(this);
+                      test2();
+                  }
+
+                  public void test1() {
+                  }
+
+                  public void test2() {
+                  }
+              }
+              """,
+            """
+              import org.mockito.MockitoAnnotations;
+              import org.junit.jupiter.api.BeforeEach;
+
+              class A {
+
+                  @BeforeEach
+                  public void setUp() {
+                      test1();
+                      AutoCloseable mocks = MockitoAnnotations.openMocks(this);
+                      test2();
+                  }
+
+                  public void test1() {
+                  }
+
+                  public void test2() {
+                  }
+              }
+              """
+          )
+        );
+    }
+}
