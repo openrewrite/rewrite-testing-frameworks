9e1d7c6c23baed0f0560b70bf161437c5979752e
Tim te Beek <tim@moderne.io>
2025-04-11 16:02:51 +0200

    Exclude dataflow-errorprone over shaded vulnerable Guava

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("org.openrewrite:rewrite-templating:${rewriteVersion}")
     compileOnly("com.google.errorprone:error_prone_core:2.+") {
         exclude("com.google.auto.service", "auto-service-annotations")
+        exclude("io.github.eisop","dataflow-errorprone")
     }
 
     testRuntimeOnly(gradleApi())
