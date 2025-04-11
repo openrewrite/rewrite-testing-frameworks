7747ad28fec895974120a957ca43f73281ccf2f0
Andrii Rodionov <andrii@moderne.io>
2025-04-11 12:54:02 +0200

    Recipe to remove initMocks if specified JUnit runners (#695)
    * Recipe to remove initMocks if specified JUnit runners
    
    * - added test and constraints
    
    * - added recipe to migration scenario
    
    * - fix code formating
    
    * - fix mockito.yml
    
    * - fix mockito.yml
    
    * - add nullability annotation
    
    * - add test with static import
    
    * - replace leading annotations, on service(AnnotationService.class)
    
    * - remove @Before/@BeforeEach imports if removing method
    
    * Update src/main/java/org/openrewrite/java/testing/mockito/RemoveInitMocksIfRunnersSpecified.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Use equality check as the implied `with` in super visitor changes tree
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/org/openrewrite/java/testing/mockito/RemoveInitMocksIfRunnersSpecified.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.mockito;
+
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.service.AnnotationService;
+import org.openrewrite.java.tree.J;
+
+public class RemoveInitMocksIfRunnersSpecified extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Remove `MockitoAnnotations.initMocks(this)` if specified JUnit runners";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Remove `MockitoAnnotations.initMocks(this)` if specified class-level JUnit runners `@RunWith(MockitoJUnitRunner.class)` or `@ExtendWith(MockitoExtension.class)`.";
+    }
+
+    private static final String MOCKITO_EXTENSION = "org.mockito.junit.jupiter.MockitoExtension";
+    private static final String MOCKITO_JUNIT_RUNNER = "org.mockito.junit.MockitoJUnitRunner";
+    private static final AnnotationMatcher MOCKITO_EXTENSION_MATCHER = new AnnotationMatcher("@org.junit.jupiter.api.extension.ExtendWith(" + MOCKITO_EXTENSION + ".class)");
+    private static final AnnotationMatcher MOCKITO_JUNIT_MATCHER = new AnnotationMatcher("@org.junit.runner.RunWith(" + MOCKITO_JUNIT_RUNNER + ".class)");
+    private static final MethodMatcher INIT_MOCKS_MATCHER = new MethodMatcher("org.mockito.MockitoAnnotations initMocks(..)", false);
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                Preconditions.and(
+                        new UsesMethod<>(INIT_MOCKS_MATCHER),
+                        Preconditions.or(
+                                new UsesType<>(MOCKITO_EXTENSION, false),
+                                new UsesType<>(MOCKITO_JUNIT_RUNNER, false)
+                        )
+                ),
+                new JavaIsoVisitor<ExecutionContext>() {
+
+                    @Override
+                    public J.@Nullable MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                        J.MethodInvocation mi = super.visitMethodInvocation(method, ctx);
+                        if (INIT_MOCKS_MATCHER.matches(mi)) {
+                            maybeRemoveImport("org.mockito.MockitoAnnotations");
+                            return null;
+                        }
+                        return mi;
+                    }
+
+                    @Override
+                    public J.@Nullable MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+                        J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
+                        if (md != method && md.getBody() != null && md.getBody().getStatements().isEmpty()) {
+                            maybeRemoveImport("org.junit.jupiter.api.BeforeEach");
+                            maybeRemoveImport("org.junit.Before");
+                            return null;
+                        }
+                        return md;
+                    }
+
+                    @Override
+                    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration cd, ExecutionContext ctx) {
+                        if (service(AnnotationService.class).matches(updateCursor(cd), MOCKITO_EXTENSION_MATCHER) ||
+                                service(AnnotationService.class).matches(updateCursor(cd), MOCKITO_JUNIT_MATCHER)) {
+                            return super.visitClassDeclaration(cd, ctx);
+                        }
+                        return cd;
+                    }
+                }
+        );
+    }
+}
---
+++ src/test/java/org/openrewrite/java/testing/mockito/RemoveInitMocksIfRunnersSpecifiedTest.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class RemoveInitMocksIfRunnersSpecifiedTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "mockito-core", "mockito-junit-jupiter", "junit-4", "junit-jupiter-api-5"))
+          .recipe(new RemoveInitMocksIfRunnersSpecified());
+    }
+
+    @Test
+    @DocumentExample
+    void removeInitMocksInJUnit5() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.BeforeEach;
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.junit.jupiter.MockitoExtension;
+              import org.mockito.MockitoAnnotations;
+
+              @ExtendWith(MockitoExtension.class)
+              class A {
+
+                  @BeforeEach
+                  public void setUp() {
+                      MockitoAnnotations.initMocks(this);
+                  }
+
+                  public void test() {
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.junit.jupiter.MockitoExtension;
+
+              @ExtendWith(MockitoExtension.class)
+              class A {
+
+                  public void test() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeInitMocksWithStaticImport() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.junit.jupiter.MockitoExtension;
+              import org.mockito.MockitoAnnotations;
+
+              import static org.mockito.MockitoAnnotations.initMocks;
+
+              @ExtendWith(MockitoExtension.class)
+              class A {
+
+                  public void setUp() {
+                      initMocks(this);
+                  }
+
+                  public void test() {
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.junit.jupiter.MockitoExtension;
+
+              @ExtendWith(MockitoExtension.class)
+              class A {
+
+                  public void test() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeInitMocksInJUnit4() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.Before;
+              import org.junit.runner.RunWith;
+              import org.mockito.junit.MockitoJUnitRunner;
+              import org.mockito.MockitoAnnotations;
+
+              @RunWith(MockitoJUnitRunner.class)
+              class A {
+
+                  @Before
+                  public void setUp() {
+                      MockitoAnnotations.initMocks(this);
+                  }
+              }
+              """,
+            """
+              import org.junit.runner.RunWith;
+              import org.mockito.junit.MockitoJUnitRunner;
+
+              @RunWith(MockitoJUnitRunner.class)
+              class A {
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void leaveEnclosingMethodIfNotEmpty() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.runner.RunWith;
+              import org.mockito.junit.MockitoJUnitRunner;
+              import org.mockito.MockitoAnnotations;
+
+              @RunWith(MockitoJUnitRunner.class)
+              class A {
+
+                  public void setUp() {
+                      MockitoAnnotations.initMocks(this);
+                      System.out.println("log");
+                  }
+              }
+              """,
+            """
+              import org.junit.runner.RunWith;
+              import org.mockito.junit.MockitoJUnitRunner;
+
+              @RunWith(MockitoJUnitRunner.class)
+              class A {
+
+                  public void setUp() {
+                      System.out.println("log");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void notRemoveInitMocksWithoutRunners() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.jupiter.api.extension.ExtendWith;
+              import org.mockito.junit.jupiter.MockitoExtension;
+              import org.mockito.MockitoAnnotations;
+
+              class A {
+
+                  public void setUp() {
+                      MockitoAnnotations.initMocks(this);
+                  }
+
+                  public void test() {
+                  }
+              }
+              """
+          )
+        );
+    }
+}
