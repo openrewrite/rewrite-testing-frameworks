88805fc59efa5489abaf207b5d93efbf267b7ddb
Anshuman Mishra <a.mishra@uber.com>
2025-04-21 00:57:28 -0700

    Utilize MockitoJUnitRunnerToExtension for UseMockitoExtension

--- src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/JUnit5MigrationTest.java
@@ -470,9 +470,13 @@ class JUnit5MigrationTest implements RewriteTest {
                 """
                   import org.junit.jupiter.api.extension.ExtendWith;
                   import org.mockito.junit.jupiter.MockitoExtension;
+                  import org.mockito.junit.jupiter.MockitoSettings;
+                  import org.mockito.quality.Strictness;
 
+                  @MockitoSettings(strictness = Strictness.WARN)
                   @ExtendWith(MockitoExtension.class)
-                  public class MyClassTest {}
+                  public class MyClassTest {
+                  }
                   """
               )
             ),
