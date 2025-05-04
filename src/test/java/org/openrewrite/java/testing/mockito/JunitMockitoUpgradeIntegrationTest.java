2c28b9e1d582054ef9f99b41e553e44e0b5b1b3a
Anshuman Mishra <119983081+amishra-u@users.noreply.github.com>
2025-05-04 12:34:13 -0700

    Utilize MockitoJUnitRunnerToExtension for UseMockitoExtension (#711)
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/java/testing/mockito/JunitMockitoUpgradeIntegrationTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/JunitMockitoUpgradeIntegrationTest.java
@@ -260,9 +260,13 @@ class JunitMockitoUpgradeIntegrationTest implements RewriteTest {
             """
               import org.junit.jupiter.api.extension.ExtendWith;
               import org.mockito.junit.jupiter.MockitoExtension;
+              import org.mockito.junit.jupiter.MockitoSettings;
+              import org.mockito.quality.Strictness;
 
+              @MockitoSettings(strictness = Strictness.WARN)
               @ExtendWith(MockitoExtension.class)
-              public class ExampleTest {}
+              public class ExampleTest {
+              }
               """
           )
         );
