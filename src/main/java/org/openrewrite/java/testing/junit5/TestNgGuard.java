172b6f0f4ce6285a0b8ebc64c852638ed06c3080
Tim te Beek <tim@moderne.io>
2025-07-28 23:53:34 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/testing/junit5/TestNgGuard.java
+++ src/main/java/org/openrewrite/java/testing/junit5/TestNgGuard.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.testing.junit5;
 
 import lombok.Value;
@@ -113,8 +128,8 @@ public class TestNgGuard extends ScanningRecipe<TestNgGuard.Accumulator> {
                     return tree;
                 }
                 if (
-                    (pwoTngd || acc.getProjectsWithoutTestNgDependency().isEmpty())
-                    && (pwoTngtu || acc.getProjectsWithoutTestNgTypeUsage().isEmpty())
+                    (pwoTngd || acc.getProjectsWithoutTestNgDependency().isEmpty()) &&
+                    (pwoTngtu || acc.getProjectsWithoutTestNgTypeUsage().isEmpty())
                 ) {
                     return SearchResult.found(tree);
                 }
