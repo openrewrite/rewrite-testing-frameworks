69f334c1dab503ff55c02bdcf4b7a25e5a16d19d
Tim te Beek <tim@moderne.io>
2025-09-24 22:44:54 +0200

    Revert "Fix test failures and simplify test suite"
    This reverts commit 0197e2170d1ff64e80765d4fecb5c929805c57cd.

--- src/main/java/org/openrewrite/java/testing/truth/TruthAssertWithMessageToAssertJ.java
+++ src/main/java/org/openrewrite/java/testing/truth/TruthAssertWithMessageToAssertJ.java
@@ -76,23 +76,13 @@ public class TruthAssertWithMessageToAssertJ extends Recipe {
                             }
                             if (messageArgs.size() > 1) {
                                 // Formatted message - needs to be combined
-                                Object[] formatArgs = new Object[messageArgs.size() + 1];
-                                formatArgs[0] = actual;
-                                formatArgs[1] = messageArgs.get(0);
-                                for (int i = 1; i < messageArgs.size(); i++) {
-                                    formatArgs[i + 1] = messageArgs.get(i);
-                                }
-                                String template = "assertThat(#{any()}).as(String.format(#{any()}";
-                                for (int i = 1; i < messageArgs.size(); i++) {
-                                    template += ", #{any()}";
-                                }
-                                template += "))";
-
-                                return JavaTemplate.builder(template)
+                                return JavaTemplate.builder("assertThat(#{any()}).as(String.format(#{any()}, #{}))")
                                         .staticImports("org.assertj.core.api.Assertions.assertThat")
                                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "assertj-core-3"))
                                         .build()
-                                        .apply(getCursor(), mi.getCoordinates().replace(), formatArgs);
+                                        .apply(getCursor(), mi.getCoordinates().replace(),
+                                                actual, messageArgs.get(0),
+                                                messageArgs.subList(1, messageArgs.size()).toArray());
                             }
                         }
                     }
---
+++ src/test/java/org/openrewrite/java/testing/truth/TruthThrowableAssertionsTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.truth;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class TruthThrowableAssertionsTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new TruthThrowableAssertions())
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "guava", "junit-4", "jspecify", "truth"));
+    }
+
+    @DocumentExample
+    @Test
+    void hasMessageThatContains() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception e = new IllegalArgumentException("Invalid argument provided");
+                      assertThat(e).hasMessageThat().contains("Invalid");
+                  }
+              }
+              """,
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception e = new IllegalArgumentException("Invalid argument provided");
+                      assertThat(e).hasMessageContaining("Invalid");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void hasMessageThatIsEqualTo() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception e = new RuntimeException("Error occurred");
+                      assertThat(e).hasMessageThat().isEqualTo("Error occurred");
+                  }
+              }
+              """,
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception e = new RuntimeException("Error occurred");
+                      assertThat(e).hasMessage("Error occurred");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void hasCauseThatIsInstanceOf() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception cause = new IllegalStateException("Bad state");
+                      Exception e = new RuntimeException("Wrapper", cause);
+                      assertThat(e).hasCauseThat().isInstanceOf(IllegalStateException.class);
+                  }
+              }
+              """,
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception cause = new IllegalStateException("Bad state");
+                      Exception e = new RuntimeException("Wrapper", cause);
+                      assertThat(e).hasCauseInstanceOf(IllegalStateException.class);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void multipleThrowableAssertions() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception cause = new IllegalStateException("Inner error");
+                      Exception e = new RuntimeException("Outer error", cause);
+
+                      assertThat(e).hasMessageThat().contains("Outer");
+                      assertThat(e).hasMessageThat().isEqualTo("Outer error");
+                      assertThat(e).hasCauseThat().isInstanceOf(IllegalStateException.class);
+                  }
+              }
+              """,
+            """
+              import static com.google.common.truth.Truth.assertThat;
+
+              class Test {
+                  void test() {
+                      Exception cause = new IllegalStateException("Inner error");
+                      Exception e = new RuntimeException("Outer error", cause);
+
+                      assertThat(e).hasMessageContaining("Outer");
+                      assertThat(e).hasMessage("Outer error");
+                      assertThat(e).hasCauseInstanceOf(IllegalStateException.class);
+                  }
+              }
+              """
+          )
+        );
+    }
+}
