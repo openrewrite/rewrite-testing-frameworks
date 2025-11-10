d732c2bbee4a5b1b92fa61365d8adcda1c5c0f31
Tim te Beek <tim@moderne.io>
2025-11-10 21:44:13 +0100

    Move recipe and include with v2 upgrade

--- src/test/java/org/openrewrite/java/testing/testcontainers/AddTestcontainersAnnotationsTest.java
+++ src/test/java/org/openrewrite/java/testing/testcontainers/AddTestcontainersAnnotationsTest.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.testing.junit5;
-
-import static org.openrewrite.java.Assertions.java;
+package org.openrewrite.java.testing.testcontainers;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
@@ -23,6 +21,8 @@ import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
 
 class AddTestcontainersAnnotationsTest implements RewriteTest {
 
