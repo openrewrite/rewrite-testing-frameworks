ea2384db5b4e2891f5ff82d32862355a4b56f0f5
Tim te Beek <tim@moderne.io>
2025-11-10 21:40:53 +0100

    Polish

--- src/main/java/org/openrewrite/java/testing/junit5/AddTestcontainersAnnotations.java
+++ src/main/java/org/openrewrite/java/testing/junit5/AddTestcontainersAnnotations.java
@@ -19,17 +19,16 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.service.AnnotationService;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.TypeUtils;
 
-import java.util.List;
-
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
 
 /**
  * An OpenRewrite recipe that migrates JUnit 4 Testcontainers {@code @Rule} or {@code @ClassRule}
@@ -60,71 +59,54 @@ public class AddTestcontainersAnnotations extends Recipe {
                 new UsesType<>(CLASS_RULE_FQN, true)
         );
         return Preconditions.check(usesRule, new JavaIsoVisitor<ExecutionContext>() {
-            private boolean isRule(J.VariableDeclarations varDecls) {
-                return varDecls.getLeadingAnnotations().stream()
-                        .map(J.Annotation::getType)
-                        .anyMatch(t -> TypeUtils.isAssignableTo(RULE_FQN, t) || TypeUtils.isAssignableTo(CLASS_RULE_FQN, t));
-            }
-
             @Override
-            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-                classDecl = super.visitClassDeclaration(classDecl, ctx);
-
-                if (!getCursor().getMessage("hasContainerRule", false)) {
-                    return classDecl;
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDeclaration, ExecutionContext ctx) {
+                J.ClassDeclaration cd = super.visitClassDeclaration(classDeclaration, ctx);
+                if (classDeclaration == cd) {
+                    return cd;
                 }
 
                 maybeRemoveImport(RULE_FQN);
                 maybeRemoveImport(CLASS_RULE_FQN);
 
-                boolean alreadyHasTestcontainersAnnotation = classDecl.getLeadingAnnotations().stream()
-                        .anyMatch(ann -> TypeUtils.isAssignableTo(TESTCONTAINERS_FQN, ann.getType()));
-
-                if (alreadyHasTestcontainersAnnotation) {
-                    return classDecl;
+                if (service(AnnotationService.class).isAnnotatedWith(cd, TESTCONTAINERS_FQN)) {
+                    return cd;
                 }
 
+                // Add class level annotation
                 maybeAddImport(TESTCONTAINERS_FQN);
-
                 return JavaTemplate.builder("@Testcontainers")
                         .imports(TESTCONTAINERS_FQN)
                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "testcontainers-1", "junit-jupiter-1"))
-                        .build().apply(updateCursor(classDecl), classDecl.getCoordinates().addAnnotation(comparing(J.Annotation::getSimpleName)));
+                        .build()
+                        .apply(updateCursor(cd), cd.getCoordinates().addAnnotation(comparing(J.Annotation::getSimpleName)));
             }
 
             @Override
             public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations varDecls, ExecutionContext ctx) {
-                if (!isRule(varDecls)) {
-                    return varDecls;
-                }
-
                 if (!TypeUtils.isAssignableTo(GENERIC_CONTAINER_FQN, varDecls.getType())) {
                     return varDecls;
                 }
-
-                // Tell first enclosing ClassDeclaration it needs to add @Testcontainers annotation
-                getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, "hasContainerRule", true);
-
-                List<J.Annotation> annotationsToKeep = varDecls.getLeadingAnnotations().stream()
-                        .filter(ann -> !TypeUtils.isAssignableTo(RULE_FQN, ann.getType()))
-                        .filter(ann -> !TypeUtils.isAssignableTo(CLASS_RULE_FQN, ann.getType()))
-                        .collect(toList());
-
-                varDecls = varDecls.withLeadingAnnotations(annotationsToKeep);
-
-                boolean alreadyHasContainerAnnotation = annotationsToKeep.stream()
-                        .anyMatch(ann -> TypeUtils.isAssignableTo(CONTAINER_FQN, ann.getType()));
-
-                if (alreadyHasContainerAnnotation) {
+                if (!service(AnnotationService.class).isAnnotatedWith(varDecls, RULE_FQN) &&
+                        !service(AnnotationService.class).isAnnotatedWith(varDecls, CLASS_RULE_FQN)) {
                     return varDecls;
                 }
 
-                maybeAddImport(CONTAINER_FQN);
+                // Remove ClassRule/Rule annotations
+                J.VariableDeclarations vd = varDecls.withLeadingAnnotations(ListUtils.filter(varDecls.getLeadingAnnotations(),
+                        ann -> !TypeUtils.isAssignableTo(RULE_FQN, ann.getType()) &&
+                                !TypeUtils.isAssignableTo(CLASS_RULE_FQN, ann.getType())));
+                if (vd == varDecls || service(AnnotationService.class).isAnnotatedWith(varDecls, CONTAINER_FQN)) {
+                    return vd;
+                }
 
+                // Add field level annotation
+                maybeAddImport(CONTAINER_FQN);
                 return JavaTemplate.builder("@Container")
                         .imports(CONTAINER_FQN)
                         .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "testcontainers-1", "junit-jupiter-1"))
-                        .build().apply(updateCursor(varDecls), varDecls.getCoordinates().addAnnotation(comparing(J.Annotation::getSimpleName)));
+                        .build()
+                        .apply(updateCursor(vd), vd.getCoordinates().addAnnotation(comparing(J.Annotation::getSimpleName)));
             }
         });
     }
