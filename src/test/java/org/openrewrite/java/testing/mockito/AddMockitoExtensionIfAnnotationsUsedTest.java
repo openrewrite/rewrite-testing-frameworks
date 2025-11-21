9666e278fcf523db79c6e7a35bf87734c615055c
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-11-21 10:25:17 +0100

    add documentation annotation

--- src/test/java/org/openrewrite/java/testing/mockito/AddMockitoExtensionIfAnnotationsUsedTest.java
+++ src/test/java/org/openrewrite/java/testing/mockito/AddMockitoExtensionIfAnnotationsUsedTest.java
@@ -17,6 +17,7 @@
 package org.openrewrite.java.testing.mockito;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
@@ -34,6 +35,7 @@ class AddMockitoExtensionIfAnnotationsUsedTest implements RewriteTest {
             .dependsOn("public class Service {}"));
     }
 
+    @DocumentExample
     @Test
     void addForMock(){
         rewriteRun(
@@ -61,6 +63,7 @@ class AddMockitoExtensionIfAnnotationsUsedTest implements RewriteTest {
           )
         );
     }
+
     @Test
     void addForCaptor(){
         rewriteRun(
