1ee3aac9e8158d86f8c965bcc05838ae3221453c
Steve Elliott <steve@moderne.io>
2025-11-24 17:20:40 -0500

    WiP 2 - am able to get `.build()` added, but seems to drop the after padding (including the newline), leading to it changing wrapping.

--- src/main/java/org/openrewrite/java/testing/junit5/UpdateMockWebServerMockResponse.java
+++ src/main/java/org/openrewrite/java/testing/junit5/UpdateMockWebServerMockResponse.java
@@ -1,15 +1,22 @@
 package org.openrewrite.java.testing.junit5;
 
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.*;
 import org.openrewrite.java.search.UsesType;
-import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.JavaType;
-import org.openrewrite.java.tree.Statement;
-import org.openrewrite.java.tree.TypeUtils;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.marker.Markers;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
+import static org.openrewrite.Tree.randomId;
 
 public class UpdateMockWebServerMockResponse extends Recipe {
     private static final String OLD_MOCKRESPONSE_FQN = "okhttp3.mockwebserver.MockResponse";
@@ -19,8 +26,9 @@ public class UpdateMockWebServerMockResponse extends Recipe {
     private static final String OLD_MOCKRESPONSE_SETSTATUS = OLD_MOCKRESPONSE_FQN + " setStatus(java.lang.String)";
     private static final MethodMatcher OLD_MOCKRESPONSE_SETSTATUS_MATCHER = new MethodMatcher(OLD_MOCKRESPONSE_SETSTATUS);
     private static final String NEW_MOCKRESPONSE_FQN = "mockwebserver3.MockResponse";
-    private static final String NEW_MOCKRESPONSE_BUILDER_FQN = NEW_MOCKRESPONSE_FQN + ".Builder";
+    private static final String NEW_MOCKRESPONSE_BUILDER_FQN = NEW_MOCKRESPONSE_FQN + "$Builder";
     private static final String NEW_MOCKRESPONSE_BUILDER_CONSTRUCTOR = NEW_MOCKRESPONSE_BUILDER_FQN + " <constructor>()";
+    private static final String OLD_MOCKWEBSERVER_ENQUEUE = "okthttp3.mockwebserver.MockWebServer enqueue(okhttp3.mockwebserver.MockResponse)";
 
     @Override
     public String getDisplayName() {
@@ -35,10 +43,24 @@ public class UpdateMockWebServerMockResponse extends Recipe {
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(new UsesType<>(OLD_MOCKRESPONSE_FQN, false), new JavaIsoVisitor<ExecutionContext>() {
+            private final Map<UUID, List<Integer>> methodInvocationsToAdjust = new HashMap<>();
+
             @Override
             public @Nullable J visit(@Nullable Tree tree, ExecutionContext ctx) {
                 J j = (J) tree;
-                // TODO: look into whether I could have used ...MockResponse$Builder to any other effect
+                j = new JavaIsoVisitor<ExecutionContext>() {
+                    @Override
+                    public J.MethodInvocation visitMethodInvocation(J.MethodInvocation methodInv, ExecutionContext ctx) {
+                        J.MethodInvocation mi = super.visitMethodInvocation(methodInv, ctx);
+                        List<Integer> indexes = IntStream.range(0, mi.getArguments().size())
+                                .filter(x -> TypeUtils.isAssignableTo(OLD_MOCKRESPONSE_FQN, mi.getArguments().get(x).getType()))
+                                .boxed().collect(toList());
+                        if (!indexes.isEmpty() && !methodInvocationsToAdjust.containsKey(mi.getId())) {
+                            methodInvocationsToAdjust.put(mi.getId(), indexes);
+                        }
+                        return mi;
+                    }
+                }.visit(j, ctx);
                 j = (J) new ChangeMethodInvocationReturnType(
                         OLD_MOCKRESPONSE_STATUS,
                         OLD_MOCKRESPONSE_FQN
@@ -56,28 +78,77 @@ public class UpdateMockWebServerMockResponse extends Recipe {
                 ).getVisitor().visit(j, ctx);
                 j = new JavaIsoVisitor<ExecutionContext>() {
                     @Override
-                    public J.NewClass visitNewClass(J.NewClass newClass, ExecutionContext ctx) {
-                        J.NewClass nc = super.visitNewClass(newClass, ctx);
-                        if (new MethodMatcher(NEW_MOCKRESPONSE_BUILDER_CONSTRUCTOR).matches(nc)) {
-                            maybeAddImport(NEW_MOCKRESPONSE_FQN);
-                            maybeRemoveImport(NEW_MOCKRESPONSE_BUILDER_FQN);
-                            return JavaTemplate
-                                    .builder("new MockResponse.Builder()")
-                                    .imports("mockwebserver3.MockResponse")
-                                    .javaParser(JavaParser.fromJavaVersion().classpath("mockwebserver3"))
-                                    .build()
-                                    .apply(getCursor(), nc.getCoordinates().replace());
+                    public J.MethodInvocation visitMethodInvocation(J.MethodInvocation methodInv, ExecutionContext ctx) {
+                        J.MethodInvocation mi = super.visitMethodInvocation(methodInv, ctx);
+                        List<Integer> indexes = methodInvocationsToAdjust.getOrDefault(mi.getId(), emptyList());
+                        if (!indexes.isEmpty()) {
+                            methodInvocationsToAdjust.remove(mi.getId());
+                            List<JRightPadded<Expression>> exprs = mi.getPadding().getArguments().getPadding().getElements();
+                            //mi.getPadding().getArguments().getPadding().getElements().getFirst().getAfter()
+//                            mi.getCoordinates().replaceArguments()
+                            J.MethodInvocation updated = mi.withArguments(ListUtils.map(mi.getArguments(), (index, arg) -> {
+                                if (indexes.contains(index)) {
+//                                    J something = new JavaVisitor<ExecutionContext>() {
+//                                        @Override
+//                                        public @Nullable J visit(@Nullable Tree tree, ExecutionContext executionContext, Cursor parent) {
+//                                            return JavaTemplate
+//                                                    .builder("#{any()}.build()")
+//                                                    .build()
+//                                                    .apply(
+//                                                            new Cursor(parent, tree),
+//                                                            ((Expression) tree).getCoordinates().replace(),
+//                                                            tree
+//                                                    );
+////                                            return super.visit(tree, executionContext, parent);
+//                                        }
+//                                        //                                        @Override
+////                                        public J visit(Expression expression, ExecutionContext executionContext) {
+////                                            return JavaTemplate
+////                                                    .builder("#{any()}.build()")
+//////                                                    .contextSensitive()
+////                                                    .javaParser(JavaParser.fromJavaVersion().classpath("mockwebserver3"))
+////                                                    .build()
+////                                                    .apply(getCursor(), expression.getCoordinates().replace(), expression);
+////                                        }
+//                                    }.visit(arg, ctx, getCursor());
+                                    J.MethodInvocation inner = new J.MethodInvocation(
+                                            randomId(),
+                                            Space.EMPTY,
+                                            Markers.EMPTY,
+                                            new JRightPadded<>(arg, Space.EMPTY, arg.getMarkers()),
+                                            null,
+                                            new J.Identifier(
+                                                randomId(),
+                                                Space.EMPTY,
+                                                Markers.EMPTY,
+                                                emptyList(),
+                                                "build",
+                                                null,
+                                                null
+                                            ),
+                                            JContainer.build(Space.EMPTY, emptyList(), Markers.EMPTY),
+                                            new JavaType.Method(
+                                                    null,
+                                                    Flag.Public.getBitMask(),
+                                                    TypeUtils.asFullyQualified(arg.getType()),
+                                                    "build",
+                                                    JavaType.buildType(NEW_MOCKRESPONSE_FQN),
+                                                    emptyList(),
+                                                    null,
+                                                    null,
+                                                    null,
+                                                    null,
+                                                    null
+                                            )
+                                    );
+                                    return inner;
+//                                    return (Expression) something;
+                                }
+                                return arg;
+                            }));
+                            return updated;
                         }
-                        return nc;
-                    }
-
-                    @Override
-                    public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations varDecl, ExecutionContext ctx) {
-                        J.VariableDeclarations vd = super.visitVariableDeclarations(varDecl, ctx);
-                        if (TypeUtils.isAssignableTo(NEW_MOCKRESPONSE_BUILDER_FQN, requireNonNull(vd.getVariables().get(0).getVariableType()).getType())) {
-                            return vd.withTypeExpression(requireNonNull(((J.Identifier) vd.getTypeExpression())).withSimpleName("MockResponse.Builder"));
-                        }
-                        return vd;
+                        return mi;
                     }
                 }.visit(j, ctx);
 //                j = (J) new ChangeMethodInvocationReturnType(
