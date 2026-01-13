e9dab310f6a28842f8f8473e5d004cccf7edffd2
Christian Ortlepp <me@cortlepp.net>
2026-01-13 16:44:02 +0100

    Transform `Assume` methods to without a direct counterpart to equivalent `Assumptions` (#889)
    * test: add test for assumption rewriting (#886)
    
    * feat: implement assumption rewriting (#886)
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Polish ahead of introducing matcher
    
    * Introduce matcher, document scope
    
    * Restore original test
    
    * Remove two argument rule; doesn't work and shouldn't
    
    * fix: recipe description
    
    * Update recipes.csv
    
    ---------
    
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/main/java/org/openrewrite/java/testing/junit5/AssumeNotNullToAssumptions.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.junit5;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import org.junit.Assume;
+import org.junit.jupiter.api.Assumptions;
+import org.openrewrite.java.template.Matcher;
+import org.openrewrite.java.template.Matches;
+import org.openrewrite.java.template.RecipeDescriptor;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.stream.Stream;
+
+@RecipeDescriptor(
+        name = "Transform `Assume` methods to `Assumptions`",
+        description = "Transform `Assume` methods without a direct counterpart to equivalent assumptions in `Assumptions`.")
+public class AssumeNotNullToAssumptions {
+
+    @RecipeDescriptor(
+            name = "Transform singlar `assumeNotNull(object)` to `assumeFalse(object == null)`",
+            description = "Transform singlar `Assume.assumeNotNull(object)` to `Assumptions.assumeFalse(object == null)`.")
+    static class SingleArg {
+        @BeforeTemplate
+        void before(@Matches(SingleArgMatcher.class) Object object) {
+            Assume.assumeNotNull(object);
+        }
+
+        @AfterTemplate
+        void after(Object object) {
+            Assumptions.assumeFalse(object == null);
+        }
+    }
+
+    @RecipeDescriptor(
+            name = "Transform variadic `assumeNotNull(objects...)` to a stream of `assumeFalse(object == null)`",
+            description = "Transform `Assume.assumeNotNull(objects...)` to `Stream.of(object1, object2).forEach(o -> Assumptions.assumeFalse(o == null))`.")
+    static class VarArgs {
+        @BeforeTemplate
+        void before(Object[] objects) {
+            Assume.assumeNotNull(objects);
+        }
+
+        @AfterTemplate
+        void after(Object[] objects) {
+            Stream.of(objects).forEach(o -> Assumptions.assumeFalse(o == null));
+        }
+    }
+
+    public static class SingleArgMatcher implements Matcher<Expression> {
+        @Override
+        public boolean matches(Expression e) {
+            return TypeUtils.asArray(e.getType()) == null;
+        }
+    }
+}
--- src/test/java/org/openrewrite/java/testing/junit5/AssumeToAssumptionsTest.java
+++ src/test/java/org/openrewrite/java/testing/junit5/AssumeToAssumptionsTest.java
@@ -31,17 +31,14 @@ class AssumeToAssumptionsTest implements RewriteTest {
     public void defaults(RecipeSpec spec) {
         spec
           .parser(JavaParser.fromJavaVersion()
-            .classpathFromResources(new InMemoryExecutionContext(), "junit-4", "hamcrest-3"));
+            .classpathFromResources(new InMemoryExecutionContext(), "junit-4"))
+          .recipeFromResources("org.openrewrite.java.testing.junit5.JUnit5BestPractices");
     }
 
     @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/54")
     @Test
     void assumeToAssumptions() {
         rewriteRun(
-          spec -> spec.recipe(Environment.builder()
-            .scanRuntimeClasspath("org.openrewrite.java.testing")
-            .build()
-            .activateRecipes("org.openrewrite.java.testing.junit5.JUnit5BestPractices")),
           //language=java
           java(
             """
