5197e922b33744b35f2039bf5463296e7b357c76
Tim te Beek <tim@moderne.io>
2026-01-13 14:44:05 +0100

    Introduce matcher, document scope

--- src/main/java/org/openrewrite/java/testing/junit5/AssumeNotNullToAssumptions.java
+++ src/main/java/org/openrewrite/java/testing/junit5/AssumeNotNullToAssumptions.java
@@ -19,21 +19,25 @@ import com.google.errorprone.refaster.annotation.AfterTemplate;
 import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import org.junit.Assume;
 import org.junit.jupiter.api.Assumptions;
+import org.openrewrite.java.template.Matcher;
+import org.openrewrite.java.template.Matches;
 import org.openrewrite.java.template.RecipeDescriptor;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.TypeUtils;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 @RecipeDescriptor(
         name = "Transform `Assume` methods to `Assumptions`",
         description = "Transform `Assume` methods to without a direct counterpart to equivalent assumptions in `Assumptions`.")
-public class AssumeToAssumptions {
+public class AssumeNotNullToAssumptions {
 
     @RecipeDescriptor(
             name = "Transform singlar `assumeNotNull(object)` to `assumeFalse(object == null)`",
             description = "Transform singlar `Assume.assumeNotNull(object)` to `Assumptions.assumeFalse(object == null)`.")
-    static class AssumeNotNull {
+    static class SingleArg {
         @BeforeTemplate
-        void before(Object object) {
+        void before(@Matches(SingleArgMatcher.class) Object object) {
             Assume.assumeNotNull(object);
         }
 
@@ -44,17 +48,39 @@ public class AssumeToAssumptions {
     }
 
     @RecipeDescriptor(
-            name = "Transform variadic `assumeNotNull(objects...)` to a stream of `assumeFalse(object == null)`",
-            description = "Transform `Assume#assumeNotNull` to an Assumption")
-    static class AssumeNotNullVariadic {
+            name = "Transform two-argument `assumeNotNull` to a stream of `assumeFalse(object == null)`",
+            description = "Transform `Assume.assumeNotNull(object1, object2)` to `Stream.of(object1, object2).forEach(o -> Assumptions.assumeFalse(o == null))`.")
+    static class TwoArgs {
         @BeforeTemplate
-        void before(Object[] object) {
-            Assume.assumeNotNull(object);
+        void before(Object object1, String object2) {
+            Assume.assumeNotNull(object1, object2);
         }
 
         @AfterTemplate
-        void after(Object[] object) {
-            Arrays.stream(object).forEach(o -> Assumptions.assumeFalse(o == null));
+        void after(Object object1, String object2) {
+            Stream.of(object1, object2).forEach(o -> Assumptions.assumeFalse(o == null));
+        }
+    }
+
+    @RecipeDescriptor(
+            name = "Transform variadic `assumeNotNull(objects...)` to a stream of `assumeFalse(object == null)`",
+            description = "Transform `Assume.assumeNotNull(objects...)` to `Stream.of(object1, object2).forEach(o -> Assumptions.assumeFalse(o == null))`.")
+    static class VarArgs {
+        @BeforeTemplate
+        void before(Object objects) {
+            Assume.assumeNotNull(objects);
+        }
+
+        @AfterTemplate
+        void after(Object[] objects) {
+            Stream.of(objects).forEach(o -> Assumptions.assumeFalse(o == null));
+        }
+    }
+
+    public static class SingleArgMatcher implements Matcher<Expression> {
+        @Override
+        public boolean matches(Expression e) {
+            return TypeUtils.asArray(e.getType()) == null;
         }
     }
 }
