f24775ef6b8e4f68c74b38a39540beebe934f122
Tim te Beek <tim@moderne.io>
2026-01-13 14:45:27 +0100

    Restore original test

---
+++ src/test/java/org/openrewrite/java/testing/junit5/AssumeToAssumptionsTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.testing.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Issue;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class AssumeToAssumptionsTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "junit-4"))
+          .recipeFromResources("org.openrewrite.java.testing.junit5.JUnit5BestPractices");
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/54")
+    @Test
+    void assumeToAssumptions() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.junit.Assume;
+
+              class Test {
+                  void test() {
+                      Assume.assumeTrue("One is one", true);
+                  }
+              }
+              """,
+            """
+              import org.junit.jupiter.api.Assumptions;
+
+              class Test {
+                  void test() {
+                      Assumptions.assumeTrue(true, "One is one");
+                  }
+              }
+              """
+          )
+        );
+    }
+}
