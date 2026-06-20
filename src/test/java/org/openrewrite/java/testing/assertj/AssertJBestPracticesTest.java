ad9527504ae61b305a00a87541117435e320a995
Tim te Beek <tim@moderne.io>
2026-06-20 16:44:08 +0200

    Add test showing same-subject conjuncts collapse into a chain

--- src/test/java/org/openrewrite/java/testing/assertj/AssertJBestPracticesTest.java
+++ src/test/java/org/openrewrite/java/testing/assertj/AssertJBestPracticesTest.java
@@ -411,6 +411,37 @@ class AssertJBestPracticesTest implements RewriteTest {
               )
             );
         }
+
+        @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/387")
+        @Test
+        void decomposeConjunctionWithSameSubjectCollapsesToChain() {
+            rewriteRun(
+              spec -> spec.parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "junit-jupiter-api-5")),
+              //language=java
+              java(
+                """
+                  import static org.junit.jupiter.api.Assertions.assertTrue;
+
+                  class A {
+                      void test(int x) {
+                          assertTrue(x > 5 && x < 10);
+                      }
+                  }
+                  """,
+                """
+                  import static org.assertj.core.api.Assertions.assertThat;
+
+                  class A {
+                      void test(int x) {
+                          assertThat(x)
+                                  .isGreaterThan(5)
+                                  .isLessThan(10);
+                      }
+                  }
+                  """
+              )
+            );
+        }
     }
 
     @Issue("https://github.com/openrewrite/rewrite-testing-frameworks/issues/496")
